e9b3157074fe084302baa10c7561a243e23c4aef
Jorge Tomas <jotolo7@gmail.com>
2014-07-29 01:06:33 -0400

    Making first order's charts.

--- app/controllers/dashboard/overview_controller.rb
+++ app/controllers/dashboard/overview_controller.rb
@@ -2,6 +2,20 @@ module Dashboard
   class OverviewController < ApplicationController
     def index
       @users = User.all
+      @orders=Hub::Order.all
+    end
+
+    def orders_statistics
+      days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+      orders = Hub::Order.all
+      @orders_by_week_day = orders.group_by{|o| o.placed_on.wday}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [days[c[0]],c[1].count] }
+      @orders_by_hour = orders.group_by{|o| o.placed_on.to_datetime.hour}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [c[0],c[1].count] }
+    end
+
+    def revenues_statistics
+      days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+      orders = Hub::Order.all
+      @orders_by_week_day = orders.group_by{|o| o.placed_on.wday}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [days[c[0]],c[1].count] }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Cenit::Application.routes.draw do
   mount RailsAdmin::Engine => '/data', as: 'rails_admin'
   
   namespace :dashboard do
-    get '/overview', to: 'overview#index', as: 'overview'
+    get '/overview/index', to: 'overview#index', as: 'overview'
+    get '/overview/orders_statistics', to: 'overview#orders_statistics', as: 'orders_statistics'
+    get '/overview/revenues_statistics', to: 'overview#revenues_statistics', as: 'revenues_statistics'
   end
 
   namespace :cenit do
