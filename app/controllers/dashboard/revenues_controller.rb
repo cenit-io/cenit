639342aab5266efb79543c0853d612b2749e97da
Miguel Sancho <sanchocuba@gmail.com>
2014-08-03 18:11:43 -0400

    fixed compute revenue

--- app/controllers/dashboard/revenues_controller.rb
+++ app/controllers/dashboard/revenues_controller.rb
@@ -1,8 +1,7 @@
 module Dashboard
   class RevenuesController < SalesController
     def compute(value)
-       value { |x| x.totals.nil? ? 0 : x.totals.order }.round(2)
-      #value.sum { |order| order.totals.order if order && order.totals && order.totals.order }.round(2)
+       value.sum { |x| x.totals.nil? ? 0 : x.totals.order }.round(2)
     end  
   end
 end
