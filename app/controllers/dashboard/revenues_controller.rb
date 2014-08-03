340197e136aa2cd862e1c934e21f74a5cb7eb89b
Miguel Sancho <sanchocuba@gmail.com>
2014-08-03 18:10:28 -0400

    add item

--- app/controllers/dashboard/revenues_controller.rb
+++ app/controllers/dashboard/revenues_controller.rb
@@ -1,7 +1,8 @@
 module Dashboard
   class RevenuesController < SalesController
     def compute(value)
-      value.sum { |x| x.totals.nil? ? 0 : x.totals.order }.round(2)
+       value { |x| x.totals.nil? ? 0 : x.totals.order }.round(2)
+      #value.sum { |order| order.totals.order if order && order.totals && order.totals.order }.round(2)
     end  
   end
 end
--- app/controllers/dashboard/sales_controller.rb
+++ app/controllers/dashboard/sales_controller.rb
@@ -4,23 +4,24 @@ module Dashboard
     before_action :get_comparison_orders, only: [:index, :by_week_days, :by_hours]
     
     def index
-      time_line = {}
-      @orders.each {|o| time_line[o.placed_on.to_time.to_s] = o.totals.order if o.totals } 
+      @main_data = {}
+      @orders.each {|o| @main_data[o.placed_on.to_time.to_s] = o.totals.order if o.totals } 
       
-      time_line_compare = {}
-      @comparison_orders.each {|o| time_line_compare[ o.placed_on + @diff] = o.totals.order if o.totals }       
-
-      @time_line = [{:name => "#{@start_date} - #{@end_date}" ,:data => time_line },
-                    {:name => "#{@comparison_start_date} - #{@comparison_end_date}", :data => time_line_compare }]
-
+      @compare_data = {}
+      @comparison_orders.each {|o| @compare_data[ o.placed_on + @diff] = o.totals.order if o.totals }     
+      set_data
     end  
     
     def by_week_days
-      @data = @orders.group_by{|o| o.placed_on.wday}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [days[c[0]],compute(c[1])] }
+      @main_data = @orders.group_by{|o| o.placed_on.wday}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [days[c[0]],compute(c[1])] }
+      @compare_data = @comparison_orders.group_by{|o| o.placed_on.wday}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [days[c[0]],compute(c[1])]}
+      set_data
     end
      
     def by_hours
-      @data = @orders.group_by{|o| o.placed_on.hour}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [hours[c[0]],compute(c[1])] }
+      @main_data = @orders.group_by{|o| o.placed_on.hour}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [hours[c[0]],compute(c[1])] }
+      @compare_data = @comparison_orders.group_by{|o| o.placed_on.hour}.sort{|a,b| a[0]<=>b[0]}.collect{|c| [hours[c[0]],compute(c[1])] }
+      set_data
     end
     
     def compute
@@ -28,6 +29,10 @@ module Dashboard
     end   
     
     private
+      def set_data
+        @data = [{:name => "#{@start_date} / #{@end_date}" ,:data => @main_data },
+                 {:name => "#{@comparison_start_date} / #{@comparison_end_date}", :data => @compare_data }]
+      end   
     
       def get_orders(options = {})
         @start_date = options[:start_date] ||= Date.today - 3.months
