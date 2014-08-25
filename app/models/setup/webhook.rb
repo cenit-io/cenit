4fabea6847c4a761abe94c320f7f71997663a70a
Cesar Lage <kaerdsar@gmail.com>
2014-08-25 11:13:39 -0400

    recover setup classes to branch develop

---
+++ app/models/setup/webhook.rb
@@ -0,0 +1,14 @@
+module Setup
+  class Webhook
+    include Mongoid::Document
+    include Mongoid::Attributes::Dynamic
+    include Mongoid::Timestamps
+
+    field :name, type: String
+    field :path, type: String
+
+    has_and_belongs_to_many :connections, :class_name => "Setup::Connection"
+
+    index({ starred: 1 })
+  end
+end
