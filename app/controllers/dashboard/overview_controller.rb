2726852fc3ff861b9550b7460dfd3647cc705861
Miguel Sancho <sanchocuba@gmail.com>
2014-08-25 08:58:19 -0400

    was removed dashboard module

--- app/controllers/dashboard/overview_controller.rb
+++
@@ -1,9 +0,0 @@
-module Dashboard
-  class OverviewController < BaseController
-    def index
-      @revenues = Hub::Order.where(:status => 'complete').count
-      @orders = Hub::Order.count
-      @overview_chart = {:Orders => @orders, :Revenues => @revenues }
-    end
-  end
-end
