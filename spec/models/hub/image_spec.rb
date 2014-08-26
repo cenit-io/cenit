61d87af912830947d469aa06b7674adec3d41640
Miguel Sancho <sanchocuba@gmail.com>
2014-08-26 07:43:09 -0400

    update specs

--- spec/models/hub/image_spec.rb
+++ spec/models/hub/image_spec.rb
@@ -3,5 +3,4 @@ require 'spec_helper'
 describe Hub::Image do
   it { should have_fields(:url, :position, :type, :title) }
   it { should validate_presence_of(:url) }
-
 end
--- spec/models/hub/line_item_spec.rb
+++ spec/models/hub/line_item_spec.rb
@@ -1,26 +1,5 @@
 require 'spec_helper'
 
 describe Hub::LineItem do
-
-  before(:each) do
-    @attr =  [
-          {
-            "name" => "Spree T-Shirt",
-            "product_id" => "SPREE-T-SHIRT",
-            "quantity" => 1,
-            "price" => 30.0,
-            "options_attributes" => {
-            }
-          }
-        ]
-
-
-  end
-
   it { should have_fields( :name, :product_id, :quantity, :price )}
-
-  it "should create a new instance given a valid attribute" do
-    Hub::LineItem.create!(@attr)
-  end
-
 end  
