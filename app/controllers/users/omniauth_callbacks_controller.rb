c9bf555fcbaeb1db69d7ae7b48130c454517f463
Miguel Sancho <sanchocuba@gmail.com>
2015-02-01 13:58:43 -0500

    fix the creation of new account using doorkeeper

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   protect_from_forgery with: :null_session
   def doorkeeper
     oauth_data = request.env["omniauth.auth"]
-    @user = User.find_or_create_for_doorkeeper_oauth(oauth_data)
+    @user = User.find_or_initialize_for_doorkeeper_oauth(oauth_data)
     @user.update_doorkeeper_credentials(oauth_data)
     @user.save
-
+    
     if @user.persisted?
+       #current_user = @user
        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
        set_flash_message(:notice, :success, :kind => "Doorkeeper") if is_navigational_format?
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User
   scope :by_account, -> { where(account: Account.current ) }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise  :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :database_authenticatable
 
   ## Database authenticatable
   field :email,              type: String, default: ""
@@ -34,15 +33,24 @@ class User
   field :number, as: :key, type: String
   
   field :doorkeeper_uid, type: String 
-  field :doorkeeper_access_token, type: Integer 
+  field :doorkeeper_access_token, type: String
 
   before_save :ensure_token
   validates_uniqueness_of :token
 
-  def self.find_or_create_for_doorkeeper_oauth(oauth_data)
-    user = User.find_by(doorkeeper_uid: oauth_data.uid) || User.new(doorkeeper_uid: oauth_data.uid)
-    user.email = oauth_data.info.email
+  def self.find_or_initialize_for_doorkeeper_oauth(oauth_data)
+    user = User.where(email: oauth_data.info.email).first
+    user ||= User.new(email: oauth_data.info.email,password: Devise.friendly_token[0,20])
+    user.doorkeeper_uid = oauth_data.uid
     user
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.doorkeeper_data"] && session["devise.doorkeeper_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 
   def update_doorkeeper_credentials(oauth_data)
@@ -100,11 +108,11 @@ class User
       field :created_at
       field :updated_at
     end
-    edit do
-      fields :email, :password, :password_confirmation, :roles, :key, :authentication_token
+   # edit do
+   #   fields :email, :password, :password_confirmation, :roles, :key, :authentication_token
       #field :reset_password_token
-    end  
-    fields :email, :roles, :key, :authentication_token, :sign_in_count, :last_sign_in_at, :last_sign_in_ip
+  #  end  
+  #  fields :email, :roles, :key, :authentication_token, :sign_in_count, :last_sign_in_at, :last_sign_in_ip
   end  
   
 end
