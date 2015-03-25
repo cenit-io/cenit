759b746d08c778b8079d4ee97149e3bc27956a52
macarci <macarci@gmail.com>
2015-03-25 15:55:29 -0400

    Action view transformations revisited. New translations styles included.

--- app/models/setup/transformation/action_view_transform.rb
+++ app/models/setup/transformation/action_view_transform.rb
@@ -1,51 +1,45 @@
 require 'builder'
+require 'prawn-rails'
 
 module Setup
   module Transformation
     #ActionView::Template.register_template_handler(:haml, Haml::Plugin)
     class ActionViewTransform < Setup::Transformation::AbstractTransform
-      
+
       class << self
-        
+
         def run(options = {})
           transformation = options[:transformation]
           object = options[:object]
           split_style = options[:style].split('.') if options[:style].present?
-        
-          format = options[:format] ||=  split_style[0].to_sym if split_style[0].present?
+
+          format = options[:format] ||= split_style[0].to_sym if split_style[0].present?
           handler = options[:handler] ||= split_style[1].to_sym if split_style[1].present?
-        
-          if handler.present? && metaclass.instance_methods.include?( met = "run_#{handler}".to_sym )
-            send(met,transformation, object, options)
+
+          if handler.present? && metaclass.instance_methods.include?(met = "run_#{handler}".to_sym)
+            send(met, options)
           else
-            view = ActionView::Base.new
-            view.try :render, inline: transformation, formats: format, handlers: handler, locals: {object: object}
+            ActionView::Base.new.try :render, inline: transformation, formats: format, type: handler, handlers: handler, locals: options
           end
         end
-    
+
         def run_rabl(options = {})
-          format = options[:format] ||= :json
-          renderer = Rabl::Renderer.new(options[:transformation], options[:object], { format: format, root: true })
-          renderer.render
+          Rabl::Renderer.new(options[:transformation], nil, {format: options[:format], locals: options}).render
         end
-        
-        def run_builder(options = {})
-          format = options[:format] ||= :xml          
-          eval "xml = ::Builder::XmlMarkup.new(:indent => 2);" +
-            #"self.output_buffer = xml.target!;" +
-            options[:transformation] +
-            ";xml.target!;"
-        end
-        
+
         def run_haml(options = {})
-          format = options[:format] ||= :html          
-          eval Haml::Engine.new(options[:transformation]).compiler.precompiled_with_ambles([])
+          Haml::Engine.new(options[:transformation]).render(Object.new, options)
         end
+
+        # def run_prawn(options = {})
+        #   result = PrawnRails::Engine.try(:new).try :render, inline: options[:transformation], format:  options[:format],  locals: options
+        #   result
+        # end
 
         def types
           [:Export]
         end
-        
+
       end
     end
   end
