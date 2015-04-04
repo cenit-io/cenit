5b1973cfe50baf2e8ee6b06b89a5e569956cdf4c
Miguel Sancho <sanchocuba@gmail.com>
2015-04-04 18:29:55 -0400

    removed innecesaries lines in ui.js file

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,5 +1,3 @@
-// override this file in your application to add custom behaviour
-
 //= require 'rails_admin/d3'
 //= require 'rails_admin/highcharts'
 //= require 'rails_admin/triggers-box'
@@ -7,7 +5,6 @@
 //= require rails_admin/highlight_js/highlight.pack.js
 
 $(document).on('rails_admin.dom_ready', function() {
-  //hljs.initHighlightingOnLoad();
   $('pre code').each(function(i, block) {
      hljs.highlightBlock(block);
    });
