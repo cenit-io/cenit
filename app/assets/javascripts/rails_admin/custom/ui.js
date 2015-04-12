831899090e862e0662f177b7dd021a2081a846c8
Miguel Sancho <sanchocuba@gmail.com>
2015-04-12 12:12:08 -0400

    add codemirror for edit json when in schema creation

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,11 +1,23 @@
-//= require 'rails_admin/d3'
-//= require 'rails_admin/highcharts'
-//= require 'rails_admin/triggers-box'
-//= require 'rails_admin/test-flow-transformation'
-//= require rails_admin/highlight_js/highlight.pack.js
+//= require rails_admin/d3
+//= require rails_admin/highcharts
+//= require rails_admin/triggers-box
+//= require rails_admin/test-flow-transformation
+// require rails_admin/highlight_js/highlight.pack.js
+//= require codemirror
+//= require codemirror/modes/javascript
+
+//$(document).on('rails_admin.dom_ready', function() {
+//  $('pre code').each(function(i, block) {
+//     hljs.highlightBlock(block);
+//   });
+//});
 
 $(document).on('rails_admin.dom_ready', function() {
-  $('pre code').each(function(i, block) {
-     hljs.highlightBlock(block);
+  var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+     matchBrackets: true,
+     autoCloseBrackets: true,
+     mode: "application/ld+json",
+     lineWrapping: true,
+     lineNumbers: true,
    });
 });
