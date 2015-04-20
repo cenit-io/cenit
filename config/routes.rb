8f20819367e73f7d46d187851093ac201c444d96
Miguel Sancho <sanchocuba@gmail.com>
2015-04-20 16:28:51 -0400

    doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Cenit::Application.routes.draw do
   mount RailsAdmin::Engine => '/data', as: 'rails_admin'
   root to: 'rails_admin/main#dashboard'
+  # root to: 'home#index'
 
   get 'schema', to: 'schema#index'
   get '/file/:model/:field/:id/:file', to: 'file#index'
 
   get 'explore/:api', to: 'api#explore', as: :explore_api
+  post 'write/:api', to: 'api#write', as: :write_api
+  
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } do
     get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
