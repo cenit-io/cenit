dd477afcd67858d157b3bd7071b071e3f5cff130
macarci <macarci@gmail.com>
2015-06-03 11:17:15 -0400

    Allowing to destroy nested attributes via rails admin

--- app/models/setup/webhook.rb
+++ app/models/setup/webhook.rb
@@ -24,7 +24,7 @@ module Setup
     validates_presence_of :name, :path, :purpose
     validates_uniqueness_of :name
 
-    accepts_nested_attributes_for :parameters, :headers, :template_parameters
+    accepts_nested_attributes_for :parameters, :headers, :template_parameters, allow_destroy: true
 
     def template_parameters_hash
       hash = {}
