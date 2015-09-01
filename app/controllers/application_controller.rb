d6ab9a32895ab055d903d544fb8c14669574403c
macarci <macarci@gmail.com>
2015-09-01 12:33:56 -0400

    Optimize data type handling in every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :null_session,
-    if: Proc.new { |c| c.request.format =~ %r{application/json} }
+                       if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
   rescue_from CanCan::AccessDenied do |exception|
-     redirect_to main_app.root_path, :alert => exception.message
+    redirect_to main_app.root_path, :alert => exception.message
   end
 
   def doorkeeper_oauth_client
@@ -17,23 +17,28 @@ class ApplicationController < ActionController::Base
     @token ||= OAuth2::AccessToken.new(doorkeeper_oauth_client, current_user.doorkeeper_access_token) if current_user
   end
 
-  around_filter :scope_current_account
+  around_filter :scope_current_account, :optimize_data_type_handling
 
   private
 
-    def scope_current_account
-      if current_user && current_user.account.nil?
-         current_user.add_role(:admin) unless current_user.has_role?(:admin)
-         current_user.account = Account.create_with_owner(owner: current_user)
-         current_user.save(validate: false)
-       end 
-      Account.current = current_user.account if signed_in?
-      yield
-    ensure
-      Account.current = nil
+  def optimize_data_type_handling
+    Thread.current[:data_type_optimizer] = Setup::DataTypeOptimizer.new
+    yield
+  end
+
+  def scope_current_account
+    if current_user && current_user.account.nil?
+      current_user.add_role(:admin) unless current_user.has_role?(:admin)
+      current_user.account = Account.create_with_owner(owner: current_user)
+      current_user.save(validate: false)
     end
-    
-    def after_sign_out_path_for(resource_or_scope)
-      ENV['SING_OUT_URL'] || root_path
-    end
+    Account.current = current_user.account if signed_in?
+    yield
+  ensure
+    Account.current = nil
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    ENV['SING_OUT_URL'] || root_path
+  end
 end
