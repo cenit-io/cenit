33e9a5fd965322f98a69191a0b0d40fb2a8f8e82
macarci <macarci@gmail.com>
2015-10-20 13:56:02 -0400

    Merge branch 'develop' of /home/mac/cenit with conflicts.

--- lib/mongoff/criteria.rb
+++ lib/mongoff/criteria.rb
@@ -1,33 +1,20 @@
 module Mongoff
   class Criteria
     include Enumerable
-    include Origin::Queryable
+    include Queryable
+
+    undef_method(:sort)
 
     attr_reader :model
+    attr_reader :query
 
-    def initialize(model)
-      super(Aliases.new(model), Hash.new { |h, k| h[k] = Serializer.new(model, k) })
+    def initialize(model, query)
       @model = model
+      @query = query
     end
 
     def count
       query.count
-    end
-
-    def size
-      count
-    end
-
-    def query
-      q = model.collection.find(selector)
-      options.each do |option, criterion|
-        q = q.send(option, criterion)
-      end
-      q
-    end
-
-    def size
-      count
     end
 
     def size
@@ -48,53 +35,7 @@ module Mongoff
     end
 
     def method_missing(symbol, *args)
-      if (q = query).respond_to?(symbol)
-        q.send(symbol, *args)
-      else
-        super
-      end
-    end
-
-    private
-
-    class Aliases
-
-      attr_reader :model
-
-      def initialize(model)
-        @model = model
-      end
-
-      def [](field)
-        model.attribute_key(field)
-      end
-    end
-
-    class Serializer
-
-      attr_reader :model
-      attr_reader :field
-
-      def initialize(model, field)
-        @model = model
-        @field = model.properties.detect { |key| model.attribute_key(key).to_s == field } || field.to_sym
-      end
-
-      def localized?
-        false
-      end
-
-      def evolve(value)
-        if value.respond_to?(:orm_model) && value.respond_to?(:id)
-          value.id
-        elsif value.is_a?(Hash)
-          value
-        elsif value.is_a?(Enumerable)
-          value.collect { |v| model.mongo_value(v, field) }
-        else
-          model.mongo_value(value, field)
-        end
-      end
+      query_for(model, query, symbol, *args)
     end
   end
 end
