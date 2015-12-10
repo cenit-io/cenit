fd21cc24cd5f111b4fec58630c208c7473c8764f
macarci <macarci@gmail.com>
2015-12-10 13:35:03 -0500

    Fixing creation methods data arguments for data types

--- lib/mongoff/data_type_methods.rb
+++ lib/mongoff/data_type_methods.rb
@@ -6,7 +6,7 @@ module Mongoff
       [:new_from_json, :new_from_xml, :new_from_edi].each do |method|
         class_eval "def #{method}(data, options = {})
           if data_type_methods.any? { |alg| alg.name == '#{method}' }
-            method_missing(:#{method}, options)
+            method_missing(:#{method}, data, options)
           else
             super
           end
