1339c2ad6064d09751da6ba9dd26493e04b54d47
macarci <macarci@gmail.com>
2015-12-24 11:35:11 -0500

    Properties uniqueness mongoff validation

--- lib/mongoff/record.rb
+++ lib/mongoff/record.rb
@@ -89,6 +89,13 @@ module Mongoff
       orm_model.fully_validate_against_schema(attributes).each do |error|
         errors.add(:base, error[:message])
       end
+      scope = orm_model.all
+      (unique_properties = orm_model.unique_properties).each { |property| scope = scope.or(property => self[property])}
+      scope.each do |record|
+        next if unique_properties.empty? || eql?(record)
+        (taken = unique_properties.select { |p| self[p] == record[p] }).each { |p| errors.add(p, 'is already taken') }
+        unique_properties.delete_if { |p| taken.include?(p) }
+      end
     end
 
     def valid?
