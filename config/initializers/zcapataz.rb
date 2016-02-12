12ae522654ad79afac4ad8cdbd9f55003175a366
macarci <macarci@gmail.com>
2016-02-12 16:01:17 -0500

    Setup Capataz disable option via application config

--- config/initializers/zcapataz.rb
+++ config/initializers/zcapataz.rb
@@ -2,6 +2,8 @@ require 'capataz/capataz'
 
 Capataz.config do
 
+  disable ENV['CAPATAZ_DISABLE']
+
   deny_declarations_of :module, :class, :yield, :self, :def, :const, :ivar, :cvar, :gvar, :return
 
   deny_invoke_of :require, :new, :create, :class, :eval, :class_eval, :instance_eval, :instance_variable_set, :instance_variable_get, :constants, :const_get, :const_set, :constantize
