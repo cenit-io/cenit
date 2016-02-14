4e97844834a391a7d986538063e4c76c9aff9cd6
macarci <macarci@gmail.com>
2016-02-14 08:12:06 -0500

    Redirecting to installed collection when pulled

--- lib/rails_admin/config/actions/pull.rb
+++ lib/rails_admin/config/actions/pull.rb
@@ -27,17 +27,23 @@ module RailsAdmin
             if @pull_request[:missing_parameters].blank? && params[:_pull]
               @pull_request = Cenit::Actions.pull(@object, @pull_request)
               if (errors = @pull_request[:errors]).blank?
-                if errors = @pull_request[:fixed_errors]
+                if (errors = @pull_request[:fixed_errors])
                   do_flash(:notice, t('admin.actions.pull_collection.fixed_errors_header'), errors)
                 end
-                redirect_to_on_success
-              else
+                collection = Setup::Collection.where(@pull_request[:collection]).first
+                if collection
+                  flash[:success] = t('admin.flash.successful', name: @model_config.label, action: t("admin.actions.#{@action.key}.done"))
+                  redirect_to rails_admin.show_path(model_name: collection.class.to_s.underscore.gsub('/', '~'), id: collection.id)
+                else
+                  redirect_to_on_success
+                end
+              elseflash[:success] = t('admin.flash.successful', name: @model_config.label, action: t("admin.actions.#{@action.key}.done"))
                 do_flash(:error, t('admin.flash.error', name: @model_config.label, action: t("admin.actions.#{@action.key}.done").html_safe), errors)
                 redirect_to back_or_index
               end
             else
               if params[:_pull]
-                flash[:error] = t('admin.actions.pull_collection.missing_parameters') if  @pull_request[:missing_parameters].present?
+                flash[:error] = t('admin.actions.pull_collection.missing_parameters') if @pull_request[:missing_parameters].present?
               else
                 @pull_request[:missing_parameters] = []
               end
