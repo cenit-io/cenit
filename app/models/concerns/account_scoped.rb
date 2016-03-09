89377af021e4c9632d7a5fccb1a5bb3104d2e94e
D.H. Bahr <dhbahr@gmail.com>
2016-03-09 16:16:48 -0500

    First attempt on multi-databases

--- app/models/concerns/account_scoped.rb
+++ app/models/concerns/account_scoped.rb
@@ -1,6 +1,22 @@
 module AccountScoped
   extend ActiveSupport::Concern
   included do
-    store_in collection: Proc.new { Account.tenant_collection_name(to_s) }
+    store_in client: Proc.new {
+      name = 'dhbahr-test' # TODO: get actual DB_NAME from Account
+
+      clients = {}
+
+      unless Mongoid::Clients.clients.has_key? name
+        uri = 'mongodb://localhost:27018/dhbahr_cenit_dev' # TODO: get actual DB_URI from Account
+
+        clients = Mongoid::Config.clients
+        clients[name] = {uri: uri}
+
+        Mongoid::Config.load_configuration({options: {}, clients: clients})
+      end
+
+      name
+    }
+    # store_in collection: Proc.new { Account.tenant_collection_name(to_s) }
   end
 end
