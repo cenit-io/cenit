f81033e46aead92a5e23280ed1e27898cb697da9
macarci <macarci@gmail.com>
2016-03-23 12:52:09 -0400

    Factorizing parameters

--- app/models/setup/webhook.rb
+++ app/models/setup/webhook.rb
@@ -5,25 +5,21 @@ module Setup
     include ParametersCommon
     include JsonMetadata
     include AuthorizationHandler
+    include Parameters
 
     BuildInDataType.regist(self).referenced_by(:namespace, :name).excluding(:connection_roles)
-
-    embeds_many :parameters, class_name: Setup::Parameter.to_s, inverse_of: :webhook
-    embeds_many :headers, class_name: Setup::Parameter.to_s, inverse_of: :webhook
-
-    embeds_many :template_parameters, class_name: Setup::Parameter.to_s, inverse_of: :webhook
 
     field :path, type: String
     field :method, type: String, default: :post
     field :description, type: String
+
+    parameters :parameters, :headers, :template_parameters
 
     def method_enum
       [:get, :post, :put, :delete, :patch, :copy, :head, :options, :link, :unlink, :purge, :lock, :unlock, :propfind]
     end
 
     validates_presence_of :path
-
-    accepts_nested_attributes_for :parameters, :headers, :template_parameters, allow_destroy: true
 
     def conformed_path(options = {})
       conform_field_value(:path, options)
