f8fe98049b95137e5ef1f594fa0480e43ff9c022
macarci <macarci@gmail.com>
2016-04-11 17:47:46 -0400

    Fixing Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Cenit::Application.routes.draw do
     get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
   
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
-
   root to: 'rails_admin/main#dashboard'
 
   get 'explore/:api', to: 'api#explore', as: :explore_api
@@ -36,8 +34,10 @@ Cenit::Application.routes.draw do
      end
   end
 
-  get '/:model_name/*id', to: 'rails_admin/main#show'
-
   match 'app/:ns/:app_slug' => 'app#index', via: [:all]
   match 'app/:ns/:app_slug/*path' => 'app#index', via: [:all]
+
+  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+
+  get '/:model_name/*id', to: 'rails_admin/main#show'
 end
