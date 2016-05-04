8e25e1368b1f6ec43a533634e89a24c9b3fbd6b0
Maria Esther Guirola <marygh3008@gmail.com>
2016-05-04 14:57:08 -0400

    Added swager_json to BuildInDataType

--- app/models/setup/shared_collection.rb
+++ app/models/setup/shared_collection.rb
@@ -6,7 +6,7 @@ module Setup
 
     Setup::Models.exclude_actions_for self, :copy, :new, :edit, :translator_update, :convert, :send_to_flow, :delete_all, :delete, :import
 
-    BuildInDataType.regist(self).with(:name, :shared_version, :authors, :summary, :pull_parameters, :dependencies, :data, :readme).referenced_by(:name, :shared_version)
+    BuildInDataType.regist(self).with(:name, :shared_version, :authors, :summary, :pull_parameters, :dependencies, :data, :readme, :swagger_json).referenced_by(:name, :shared_version)
 
     belongs_to :shared_name, class_name: Setup::SharedName.to_s, inverse_of: nil
 
