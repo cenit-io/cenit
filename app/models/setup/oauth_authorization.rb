2f9e48bd37472ee6304e0a6426fb97a3c96a5b55
macarci <macarci@gmail.com>
2016-05-04 16:36:37 -0400

    Refresh token strategy for OAuth 1.0 providers

--- app/models/setup/oauth_authorization.rb
+++ app/models/setup/oauth_authorization.rb
@@ -5,14 +5,16 @@ module Setup
     BuildInDataType.regist(self).with(:namespace, :name, :provider, :client, :parameters).referenced_by(:namespace, :name)
 
     field :access_token_secret, type: String
-    field :realm, type: String
+    field :realm_id, type: String
 
-    auth_template_parameters oauth_token: :access_token,
+    auth_template_parameters oauth_token: ->(oauth_auth) { oauth_auth.fresh_access_token },
                              oauth_token_secret: :access_token_secret
 
     def build_auth_header(template_parameters)
       self.class.auth_header(template_parameters.reverse_merge(consumer_key: client.attributes[:identifier],
-                                                               consumer_secret: client.attributes[:secret]))
+                                                               consumer_secret: client.attributes[:secret],
+                                                               oauth_token: access_token,
+                                                               oauth_token_secret: access_token_secret))
     end
 
     def callback_key
@@ -50,17 +52,40 @@ module Setup
       self.authorized_at = Time.now
     end
 
+    def accept_callback?(params)
+      params[:oauth_token].present? && params[:oauth_verifier].present?
+    end
+
     class << self
       def auth_header(template_parameters)
         template_parameters = template_parameters.with_indifferent_access
-        consumer = OAuth::Consumer.new(template_parameters[:consumer_key], template_parameters[:consumer_secret], site: template_parameters[:url], scheme: :header)
-        token_hash = {oauth_token: template_parameters[:oauth_token], oauth_token_secret: template_parameters[:oauth_token_secret]}
-        access_token =  OAuth::AccessToken.from_hash(consumer, token_hash)
-        path = template_parameters[:path] + '?' + template_parameters[:query]
+
+        consumer = OAuth::Consumer.new(template_parameters[:consumer_key],
+                                       template_parameters[:consumer_secret],
+                                       site: template_parameters[:url],
+                                       scheme: :header)
+
+        access_token = OAuth::AccessToken.from_hash(consumer,
+                                                    oauth_token: template_parameters[:oauth_token],
+                                                    oauth_token_secret: template_parameters[:oauth_token_secret])
+
+        path = template_parameters[:path]
+        if (query = template_parameters[:query]).present?
+          path += '?' + query
+        end
         path = '/' + path unless path.start_with?('/')
-        request = consumer.send(:create_http_request, template_parameters[:method], path, template_parameters[:body])
-        request.content_type = 'application/x-www-form-urlencoded'
-        consumer.sign!(request, access_token, {})
+
+        if (content_type = template_parameters[:contentType])
+          request = consumer.send(:create_http_request, template_parameters[:method], path, template_parameters[:body])
+          request.content_type = content_type
+          consumer.sign!(request, access_token, {})
+        else
+          request = consumer.create_signed_request(template_parameters[:method],
+                                                   path,
+                                                   access_token,
+                                                   {},
+                                                   template_parameters[:body])
+        end
         request['authorization']
       end
     end
