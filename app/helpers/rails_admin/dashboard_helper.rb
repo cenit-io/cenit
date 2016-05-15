492cda705727ba738843db376a05f120d9cca782
Miguel Sancho <sanchojaf@gmail.com>
2016-05-15 10:51:52 -0400

    added search shared collection to dashboard

---
+++ app/helpers/rails_admin/dashboard_helper.rb
@@ -0,0 +1,15 @@
+module RailsAdmin
+  module DashboardHelper
+    def percent(count = 0, max)
+      count > 0 ? (max <= 1 ? count : ((Math.log(count+1) * 100.0) / Math.log(max+1)).to_i) : -1
+    end
+    
+    def animate_length(percent)
+      [1.0, percent].max.to_i * 20
+    end
+    
+    def animate_width_to(percent)
+      "#{[2.0, percent].max.to_i}%"
+    end
+  end
+end
