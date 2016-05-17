e0900837727e2c05ad943816399ef29b0daa3037
macarci <macarci@gmail.com>
2016-05-17 12:05:31 -0400

    Database locker

---
+++ lib/cenit/locker.rb
@@ -0,0 +1,41 @@
+module Cenit
+  module Locker
+    class << self
+
+      def lock(obj)
+        collection.insert_one(_id: obj_id(obj))
+        true
+      rescue
+        false
+      end
+
+      def locked?(obj)
+        collection.find(_id: obj_id(obj)).present?
+      end
+
+      def unlock(obj)
+        if (query = collection.find(_id: obj_id(obj))).present?
+          query.delete_one
+          true
+        else
+          false
+        end
+      end
+
+      def clear
+        collection.drop
+      end
+
+      private
+
+      def obj_id(obj)
+        id = obj.respond_to?(:id) ? obj.id.to_s : obj.to_s
+        "#{obj.class.to_s.collectionize.singularize}_#{id}"
+      end
+
+      def collection
+        Mongoid.default_client[:locker]
+      end
+    end
+  end
+end
