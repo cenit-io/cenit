4b5edd09b3ee5f4f2619cfa09c455b32cd5763d5
macarci <macarci@gmail.com>
2016-06-16 13:20:54 -0400

    Refactoring user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,11 +24,11 @@ class SessionsController < Devise::SessionsController
       if token && (id_token = token.params['id_token']) &&
         (id_token = JWT.decode(id_token, nil, false, verfify_expiration: false)[0]) &&
         id_token['email_verified']
-        resource = resource_class.where(email: id_token['email']).first ||
-          resource_class.create(email: id_token['email'],
-                                confirmed_at: Time.now,
-                                password: pwd = Devise.friendly_token,
-                                password_confirmation: pwd)
+        resource = resource_class.find_or_create_by(email: id_token['email'])
+        resource.confirmed_at = Time.now
+        resource.password = pwd = Devise.friendly_token
+        resource.password_confirmation= pwd
+        resource.save
         set_flash_message(:notice, :signed_in) if is_flashing_format?
         yield resource if block_given?
         state = JSON.parse(params[:state]) rescue {}
