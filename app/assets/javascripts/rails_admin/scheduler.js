8ca0d5df0d08b0f956b9ac45e154d63a522a333b
D.H. Bahr <dhbahr@gmail.com>
2016-06-24 16:07:30 -0400

    Schedulers fixes

--- app/assets/javascripts/rails_admin/scheduler.js
+++ app/assets/javascripts/rails_admin/scheduler.js
@@ -129,7 +129,7 @@ function schedulerInit() {
 
             var dval = $("#days_sl").val();
             if (dval == "1") {
-                res["week_days"] = _.filter(_.range(0, 7), function (e) {
+                res["weeks_days"] = _.filter(_.range(0, 7), function (e) {
                     return $("#week_day_" + e).hasClass("btn-primary");
                 });
 
@@ -138,7 +138,7 @@ function schedulerInit() {
                 });
                 res["last_week_in_month"] = $('#last_week_in_month').hasClass("btn-primary");
             } else {
-                res["month_days"] = _.filter(_.range(0, 31), function (e) {
+                res["months_days"] = _.filter(_.range(0, 31), function (e) {
                     return $("#months_day_" + e).hasClass("btn-primary");
                 });
                 res["last_day_in_month"] = $('#last_day_in_month').hasClass("btn-primary");
