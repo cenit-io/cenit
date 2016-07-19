c6b091a8956f4f613b6970b21d8e711ec605eb45
macarci <macarci@gmail.com>
2016-07-19 08:42:12 -0400

    Validating before submit algorithm

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1856,9 +1856,7 @@ RailsAdmin.config do |config|
           f.scope_symbol == :evaluation
         end
         associated_collection_scope do
-          Proc.new { |scope|
-            scope.where(:parameters.with_size => 1)
-          }
+          Proc.new { |scope| scope.where(:parameters.with_size => 1) }
         end
         help I18n.t('admin.form.required')
       end
@@ -1893,6 +1891,9 @@ RailsAdmin.config do |config|
           f = bindings[:object]
           (t = f.translator) && [:Import].include?(t.type) &&
             ((f.persisted? || f.custom_data_type_selected? || f.data_type) && (t.type == :Import || f.event.blank? || f.data_type.blank? || f.data_type_scope.present?))
+        end
+        associated_collection_scope do
+          Proc.new { |scope| scope.where(:parameters.with_size => 1) }
         end
       end
       field :response_translator do
