e140b85a1c738c2358116028e6fedf698880927b
D.H. Bahr <dhbahr@gmail.com>
2016-07-19 12:25:54 -0400

    Further improvements

--- app/helpers/rails_admin/dashboard_helper.rb
+++ app/helpers/rails_admin/dashboard_helper.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 module RailsAdmin
   module DashboardHelper
     def percent(count = 0, max)
@@ -13,7 +15,16 @@ module RailsAdmin
     end
 
     def recent_users()
-      User.order(created_at: :desc).limit(5)
+      User.order(created_at: :desc).limit(10).sample(5)
+    end
+
+    def gravatar(user)
+      gravatar_check = "//gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}.png?d=404"
+      uri = URI.parse(gravatar_check)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new("/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}.png?d=404")
+      response = http.request(request)
+      response.code.to_i != 404 # from d=404 parameter
     end
   end
 end
