47fad2b462486f9d3751e085090610be6c495691
macarci <macarci@gmail.com>
2016-11-08 13:50:13 -0500

    Fixing referenced keys for transformation subclasses

--- app/models/setup/converter.rb
+++ app/models/setup/converter.rb
@@ -4,7 +4,7 @@ module Setup
     transformation_type :Conversion
     allow :new
 
-    build_in_data_type.with(:namespace, :name, :source_data_type, :target_data_type, :discard_events,:style,:source_handler, :snippet, :source_exporter, :target_importer, :discard_chained_records)
+    build_in_data_type.with(:namespace, :name, :source_data_type, :target_data_type, :discard_events,:style,:source_handler, :snippet, :source_exporter, :target_importer, :discard_chained_records).referenced_by(:namespace, :name)
 
   end
 end
