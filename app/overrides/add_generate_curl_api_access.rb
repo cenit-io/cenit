2143ac1504f269a88cc641ceab9a8613b09c55c7
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-11-09 12:12:07 -0500

    Generate cURL commands.

--- app/overrides/add_generate_curl_api_access.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-    :virtual_path => 'rails_admin/main/show',
-    :name => 'add_generate_curl_api_access',
-    :insert_after => 'div.fieldset h4',
-    :partial => 'rails_admin/curl/show'
-)
