dcb25c6fac4c12efc76b67355aece774a272ced4
macarci <macarci@gmail.com>
2016-11-14 09:30:30 -0500

    Tuning collection with resources pull

--- lib/cenit/actions.rb
+++ lib/cenit/actions.rb
@@ -72,6 +72,7 @@ module Cenit
                   if item['_type']
                     record_hash['_type'] = record.class.to_s unless record_hash['_type']
                   end
+                  record_hash['_reset'] ||= item['_reset'] if item['_reset']
                   record_hash.reject! { |key, _| !item.has_key?(key) }
                   invariant = Cenit::Utility.eql_content?(record_hash, item) do |record_value, item_value|
                     (record_value.nil? && item_value.blank?) || (item_value.nil? && record_value.blank?)
