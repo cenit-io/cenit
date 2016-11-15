87ad808a64c24ab9210256af8a9264835723d4cb
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-11-15 13:14:33 -0500

    Generate cURL command for data types modules.

--- app/helpers/rails_admin/generate_curl_helper.rb
+++ app/helpers/rails_admin/generate_curl_helper.rb
@@ -9,18 +9,43 @@ module RailsAdmin
         spec_url = 'https://cenit-io.github.io/openapi/swagger.json'
         ActiveSupport::HashWithIndifferentAccess.new(JSON.parse(open(spec_url).read))
       end
+
+      api_specification_for_data_types if @@cenit_api_spec && params[:model_name].start_with?('dt')
+
+      @@cenit_api_spec
     rescue
       false
     end
 
     ###
+    # Get custom api specification for data_type model.
+    def api_specification_for_data_types
+      ns, model_name, data_type = api_model_from_data_type
+
+      @@cenit_api_spec[:paths]["/#{ns}/#{model_name}/{id}"] = {
+        get: api_data_type_spec_get(data_type),
+        delete: api_data_type_spec_delete(data_type)
+      }
+
+      @@cenit_api_spec[:paths]["/#{ns}/#{model_name}/{id}/{view}"] = {
+        get: api_data_type_spec_get_with_view(data_type)
+      }
+
+      @@cenit_api_spec[:paths]["/#{ns}/#{model_name}"] = {
+        get: api_data_type_spec_list(data_type),
+        post: api_data_type_spec_create(data_type)
+      }
+    end
+
+    ###
     # Get api specification paths for current namespace and model.
     def api_current_paths
-      if params[:model_name].present? && api_specification
-        ns, model_name = params[:model_name].split(/~/)
+      spec = api_specification
+      if params[:model_name].present? && spec
+        ns, model_name = params[:model_name].start_with?('dt') ? api_model_from_data_type : params[:model_name].split(/~/)
         pattern = Regexp.new "^/#{ns}/#{model_name}"
 
-        api_specification[:paths].select { |k, _v| k =~ pattern }
+        spec[:paths].select { |k, _v| k =~ pattern }
       else
         {}
       end
@@ -39,7 +64,9 @@ module RailsAdmin
       token, token_header, key, key_header = api_security_headers
 
       # Set value of path parameters
-      path_parameters.each { |p| path.gsub!("{#{p[:name]}}", @object.send(p[:name])) } if @object
+      path_parameters.each do |p|
+        path.gsub!("{#{p[:name]}}", @object.send(p[:name])) if @object.respond_to?(p[:name])
+      end if @object
 
       # Generate uri and command.
       command = "curl -X #{method.upcase} \\\n"
@@ -75,8 +102,8 @@ module RailsAdmin
         schema = @@cenit_api_spec[:schemes].first
         host = @@cenit_api_spec[:host].chomp('/')
       end
-      base_path = @@cenit_api_spec[:basePath].chomp('/').reverse.chomp('/').reverse
-      path = path.reverse.chomp('/').reverse
+      base_path = @@cenit_api_spec[:basePath].sub(%r{^/|/$}, '')
+      path = path.sub(%r{^/}, '')
 
       [schema, base_path, host, path]
     end
@@ -96,15 +123,101 @@ module RailsAdmin
     # Get data object from service parameters definition.
     def api_data(parameters)
       data = {}
-      parameters.each { |p| data[p[:name]] = api_default_param_value(p[:type]) }
+      parameters.each { |p| data[p[:name]] = api_default_param_value(p) }
       data
     end
 
     ###
-    # Get default value from parameter type.
-    def api_default_param_value(param_type)
+    # Get default value from parameter.
+    def api_default_param_value(param)
       values = { 'integer' => 0, 'number' => 0, 'real' => 0, 'boolean' => false, 'object' => {}, 'array' => [] }
-      values[param_type] || ''
+      param[:default] || values[param[:type]] || ''
+    end
+
+    ###
+    # Get data type service get specification.
+    def api_data_type_spec_get(data_type)
+      {
+        tags: [data_type.name],
+        summary: "Retrieve an existing #{data_type.name}",
+        description: [
+          "Retrieves the details of an existing #{data_type.name}.",
+          "You need only supply the unique #{data_type.name} identifier",
+          "that was returned upon #{data_type.name} creation."
+        ].join(' '),
+        parameters: [{ description: 'Identifier', in: 'path', name: 'id', type: 'string', required: true }]
+      }
+    end
+
+    ###
+    # Get data type service get specification.
+    def api_data_type_spec_get_with_view(data_type)
+      {
+        tags: [data_type.name],
+        summary: "Retrieve one attribute of an existing #{data_type.name}",
+        description: "Retrieves one attribute of an existing #{data_type.name}.",
+        parameters: [
+          { description: 'Identifier', in: 'path', name: 'id', type: 'string', required: true },
+          { description: 'Attribute name', in: 'path', name: 'view', type: 'string', required: true }
+        ]
+      }
+    end
+
+    ###
+    # Get data type service delete specification.
+    def api_data_type_spec_delete(data_type)
+      {
+        tags: [data_type.name],
+        summary: "Delete an existing #{data_type.name}",
+        description: "Permanently deletes an existing #{data_type.name}. It cannot be undone.",
+        parameters: [
+          { description: 'Identifier', in: 'path', name: 'id', type: 'string', required: true }
+        ]
+      }
+    end
+
+    ###
+    # Get data type service list specification.
+    def api_data_type_spec_list(data_type)
+      limit = Kaminari.config.default_per_page
+      {
+        tags: [data_type.name],
+        summary: "Retrieve all existing #{data_type.name.pluralize}",
+        description: "Retrieve all existing #{data_type.name.pluralize} you've previously created.",
+        parameters: [
+          { description: 'Page number', in: 'query', name: 'page', type: 'integer', required: false, default: 1 },
+          { description: 'Page size', in: 'query', name: 'limit', type: 'integer', required: false, default: limit },
+          { description: 'Items order', in: 'query', name: 'order', type: 'string', required: false, default: 'id' },
+          { description: 'JSON Criteria', in: 'query', name: 'where', type: 'string', required: false, default: '{}' }
+        ]
+      }
+    end
+
+    ###
+    # Get data type service create or update specification.
+    def api_data_type_spec_create(data_type)
+      code = JSON.parse(data_type.code)
+      parameters = code['properties'].map { |k, v| { in: 'query', name: k, type: v['type'], required: false } }
+
+      {
+        tags: [data_type.name],
+        summary: "Create or update an #{data_type.name}",
+        description: [
+          "Creates or updates the specified #{data_type.name}.",
+          'Any parameters not provided will be left unchanged'
+        ].join(' '),
+        parameters: [
+          { description: 'Identifier', in: 'path', name: 'id', type: 'string', required: false }
+        ] + parameters
+      }
+    end
+
+    def api_model_from_data_type
+      data_type = Setup::DataType.find(params[:model_name].from(2))
+      ns = data_type.namespace.parameterize.underscore.downcase
+      model_name = data_type.name.parameterize.underscore.downcase
+
+      [ns, model_name, data_type]
     end
   end
 end
