b82e4264895fef6581dc242cefeb8309b763afb6
Aneli Valdes Acosta <anelivaldes@gmail.com>
2016-12-10 16:23:49 -0500

    Rescue form Exception at Contact us with not connection or wrong configuration

--- app/controllers/contact_us/contacts_controller.rb
+++ app/controllers/contact_us/contacts_controller.rb
@@ -2,11 +2,17 @@ module ContactUs
   class ContactsController < ApplicationController
 
     def create
-      @contact = ContactUs::Contact.new(permited_attributes)
-      if @contact.save
-        redirect_to('/', :notice => t('admin.contact_us.notices.success'))
-      else
-        flash[:error] = t('admin.contact_us.notices.error')
+      @contact = ContactUs::Contact.new(permitted_attributes)
+      begin
+        if @contact.save
+          redirect_to('/', :notice => t('admin.contact_us.notices.success'))
+        else
+          flash[:error] = t('admin.contact_us.notices.error')
+          redirect_to root_path
+        end
+      rescue Exception => ex
+        Setup::Notification.create_from(ex)
+        flash[:error] = "#{t('admin.contact_us.notices.error')}.An Exception happened: #{ex.message}.See your Notifications for more details"
         redirect_to root_path
       end
     end
@@ -17,7 +23,7 @@ module ContactUs
 
     private
 
-    def permited_attributes
+    def permitted_attributes
       params.require(:contact_us_contact).permit(:name, :email, :message, :subject)
     end
   end
