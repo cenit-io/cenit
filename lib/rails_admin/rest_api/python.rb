6f4656c3113ac15f526c0411ec7e10641db9a987
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-12-26 15:35:30 -0500

    Add options to generate Python code to REST-API.

--- lib/rails_admin/rest_api/python.rb
+++ lib/rails_admin/rest_api/python.rb
@@ -10,7 +10,26 @@ module RailsAdmin
         data, login = vars(method, path)
 
         # Generate uri and command.
-        command = "TODO: REST-API-PYTHON"
+        command =  ""
+        command <<  "import json\n" unless data.empty?
+        command <<  "from requests import Request, Session\n"
+        command <<  "\n"
+        command <<  "uri = '#{api_uri(method, path)}'\n"
+        command << "options = {\n"
+        command << "  'headers': {\n"
+        command << "    'Content-Type': 'application/json',\n"
+        command << "    'X-User-Access-Key': '#{login ? login.key : '-'}',\n"
+        command << "    'X-User-Access-Token': '#{login ? login.token : '-'}'\n"
+        command << "  },\n"
+        command << "  'data': json.dumps(#{data.to_json})\n" unless data.empty?
+        command << "};\n"
+        command << "\n"
+        command << "session = Session()\n"
+        command << "request = Request('#{method.upcase}', uri, **options)\n"
+        command << "prepped = request.prepare()\n"
+        command << "response = session.send(prepped)\n"
+        command << "\n"
+        command << "print(response.json())\n"
 
         command
       end
