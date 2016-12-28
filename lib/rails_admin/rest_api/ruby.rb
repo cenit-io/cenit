0f47c6b57eacd02b60da073140006e2bcca5988c
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-12-28 11:40:43 -0500

    Add options to generate Ruby code to REST-API.

--- lib/rails_admin/rest_api/ruby.rb
+++ lib/rails_admin/rest_api/ruby.rb
@@ -8,9 +8,29 @@ module RailsAdmin
       def api_ruby_code(method, path)
         # Get vars definition.
         data, login = vars(method, path)
-
+        a = {
+          :h => {
+            :a => 1
+          }
+        }
         # Generate uri and command.
-        command = "TODO: REST-API-RUBY"
+        command = "require 'rest-client'\n"
+        command << "require 'json'\n"
+        command << "\n"
+        command << "uri = '#{api_uri(method, path)}'\n"
+        command << "\n"
+        command << "response = RestClient::Request.execute(\n"
+        command << "  :url => uri,\n"
+        command << "  :method => '#{method.upcase}',\n"
+        command << "  :headers => {\n"
+        command << "    'Content-Type' => 'application/json',\n"
+        command << "    'X-User-Access-Key' => 'A480067472',\n"
+        command << "    'X-User-Access-Token' => 'oj_kJJ5ochVyDP3Q82CM',\n"
+        command << "    'params' => #{data.to_json}\n" unless data.empty?
+        command << "  }\n"
+        command << ")\n"
+        command << "\n"
+        command << "puts JSON.parse(response.body)\n"
 
         command
       end
