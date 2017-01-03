b28c07eafdc93ad8621193b8e515b019194de9f5
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-01-03 12:17:37 -0500

    format php code

--- lib/rails_admin/rest_api/php.rb
+++ lib/rails_admin/rest_api/php.rb
@@ -11,25 +11,25 @@ module RailsAdmin
 
         # Generate uri and command.
         command = ""
-        command << "  $uri = '#{api_uri(method, path)}';\n"
-        command << "  $headers = array(\n"
-        command << "    'Content-Type: application/json',\n"
-        command << "    'X-User-Access-Key: #{login ? login.key : '-'}',\n"
-        command << "    'X-User-Access-Token: #{login ? login.token : '-'}'\n"
-        command << "  );\n"
+        command << "$uri = '#{api_uri(method, path)}';\n"
+        command << "$headers = array(\n"
+        command << "  'Content-Type: application/json',\n"
+        command << "  'X-User-Access-Key: #{login ? login.key : '-'}',\n"
+        command << "  'X-User-Access-Token: #{login ? login.token : '-'}'\n"
+        command << ");\n"
         command << "\n"
-        command << "  $options = array(\n"
-        command << "    'http' => array(\n"
-        command << "      'header'  => implode($headers, \"\\r\\n\"),\n"
-        command << "      'method'  => '#{method.upcase}',\n"
-        command << "      'content' => '#{data.to_json}'\n" unless data.empty?
-        command << "    )\n"
-        command << "  );\n"
+        command << "$options = array(\n"
+        command << "  'http' => array(\n"
+        command << "    'header'  => implode($headers, \"\\r\\n\"),\n"
+        command << "    'method'  => '#{method.upcase}',\n"
+        command << "    'content' => '#{data.to_json}'\n" unless data.empty?
+        command << "  )\n"
+        command << ");\n"
         command << "\n"
-        command << "  $context  = stream_context_create($options);\n"
-        command << "  $response = file_get_contents($uri, false, $context);\n"
+        command << "$context  = stream_context_create($options);\n"
+        command << "$response = file_get_contents($uri, false, $context);\n"
         command << "\n"
-        command << "  print_r(json_decode($response, true));"
+        command << "print_r(json_decode($response, true));"
 
         command
       end
