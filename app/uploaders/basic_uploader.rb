5276e7489138bf60accd391a4f52e5f176d0df94
macarci <macarci@gmail.com>
2017-01-04 16:14:17 -0500

    Enhancing execution data type

--- app/uploaders/basic_uploader.rb
+++ app/uploaders/basic_uploader.rb
@@ -8,6 +8,7 @@ class BasicUploader < CarrierWave::Uploader::Base
   def to_hash(options={})
     hash = {}
     if present?
+      hash[:storage_id] = file.grid_file.id.to_s
       hash[:url] = "#{Cenit.homepage}#{url}"
       versions.each do |key, uploader|
         hash[key] = uploader.to_hash(options)
