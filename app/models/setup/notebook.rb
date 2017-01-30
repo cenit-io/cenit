e809cd2cac373dc798d948a0480cb5aa404b6119
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-01-30 13:57:26 -0500

    Update notebook model.

--- app/models/setup/notebook.rb
+++ app/models/setup/notebook.rb
@@ -6,7 +6,15 @@ module Setup
     build_in_data_type
 
     field :name, type: String
-    field :gist_id, type: String
+    field :parent, type: String
+    field :type, type: String
+    field :content, type: String
+    field :format, type: String
+    field :mimetype, type: String
+    field :writable, type: Boolean
 
+    def type_enum
+      %w(notebook file directory)
+    end
   end
 end
