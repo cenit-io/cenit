1eb044d6679ec5862a1b810f0165b0b6988c0b99
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-02-07 16:00:53 -0500

    update notebooks path

--- app/helpers/rails_admin/notebooks_helper.rb
+++ app/helpers/rails_admin/notebooks_helper.rb
@@ -6,8 +6,9 @@ module RailsAdmin
     def notebooks_jupyter_url
       login = Account.current || User.current
       base_url = ENV['JUPYTER_NOTEBOOKS_URL'] || "http://127.0.0.1:8888"
+      ns, model_name = api_model
 
-      "#{base_url}/tree/#{login.key}/#{login.token}/#{@model_name}"
+      "#{base_url}/tree/#{login.key}/#{login.token}/#{ns}/#{model_name}"
     end
 
   end
