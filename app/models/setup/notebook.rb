fbfcd0c14e3e78a52a5266fb3eb3a92ba823eaa0
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-02-09 14:04:07 -0500

    update notebook model to support shared state.

--- app/models/setup/notebook.rb
+++ app/models/setup/notebook.rb
@@ -1,18 +1,44 @@
 module Setup
   class Notebook
-    include CenitScoped
+    include CenitUnscoped
     include Mongoid::Timestamps
 
-    build_in_data_type.with(:module, :name, :content, :created_at, :updated_at)
+    build_in_data_type.with(:module, :name, :content, :shared, :writable, :created_at, :updated_at)
 
     field :module, type: String
     field :name, type: String
     field :content, type: String
+    field :shared, type: Boolean
+    field :writable, type: Boolean
     field :created_at, type: DateTime
     field :updated_at, type: DateTime
 
+    belongs_to :owner, class_name: Cenit::MultiTenancy.user_model_name, inverse_of: nil
+
+    attr_readonly :owner, :writable
+
+    set_callback :save, :before, :before_save
+
+    default_scope -> {
+      where(nil
+      ).or(
+        owner: Cenit::MultiTenancy.tenant_model.current ? Cenit::MultiTenancy.tenant_model.current.owner : nil
+      ).or(
+        shared: true
+      )
+    }
+
     def type_enum
       %w(notebook file directory)
+    end
+
+    def before_save
+      self.shared = false if self.shared.nil?
+      self.owner ||= Cenit::MultiTenancy.tenant_model.current.owner
+    end
+
+    def writable
+      self.owner == Cenit::MultiTenancy.tenant_model.current.owner
     end
 
   end
