2750e8c63dd798d8c629a150f0dbf4fb0b6e9c33
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2017-03-01 10:13:40 -0800

    add parentheses to method parameters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def percent(count, max)
     count > 0 ? (max <= 1 ? count : ((Math.log(count+1) * 100.0) / Math.log(max+1)).to_i) : -1
   end
-  def format_name name
+  
+  def format_name(name)
     max_name_length = 30
     if name.length > max_name_length
       name = name.to(27) + '...'
