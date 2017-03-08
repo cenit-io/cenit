a55acdbccae4e10e8e730e62d3eb9ff0d21fbbc1
macarci <macarci@gmail.com>
2017-03-08 16:51:52 -0500

    Multiple changed_if blocks

--- app/models/setup/changed_if.rb
+++ app/models/setup/changed_if.rb
@@ -3,20 +3,19 @@ module Setup
     extend ActiveSupport::Concern
 
     included do
-      class_attribute :changed_if_block
+      class_attribute :changed_if_blocks
+      self.changed_if_blocks = []
     end
 
     def changed?
-      super || ((block = self.class.changed_if) && instance_eval(&block))
+      super || self.class.changed_if_blocks.any? { |block| instance_eval(&block) }
     end
 
     module ClassMethods
 
       def changed_if(&block)
         if block
-          self.changed_if_block = block
-        else
-          changed_if_block
+          self.changed_if_blocks << block
         end
       end
     end
