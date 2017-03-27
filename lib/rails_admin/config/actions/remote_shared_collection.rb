e2e45d0b95d2b2a403d2967cc6b31367164f1d10
macarci <macarci@gmail.com>
2017-03-27 15:36:26 -0400

    Showing remote shared collections

--- lib/rails_admin/config/actions/remote_shared_collection.rb
+++ lib/rails_admin/config/actions/remote_shared_collection.rb
@@ -18,23 +18,51 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
-            operation = Setup::Connection.get("#{Cenit.host}/api/v2/setup/cross_shared_collection")
+            cenit_host = Cenit.host
+            @model_config = RailsAdmin.config(Setup::CrossSharedCollection)
+            cenit_api_path = "#{cenit_host}/api/v2/setup/cross_shared_collection"
+            fields = 'id,name,title,readme,shared_version,summary,categories,description,image'
             parameters = {
+              installed: true,
               page: params[:page] || 1,
-              only: 'id,name,title,readme,shared_version,summary,categories,description,image',
               limit: @limit = 20
             }
-            if (@query = params[:query].to_s.presence)
-              parameters['$or'] = %w{name title readme summary description}.collect { |field| { field => { '$regex': @query } } }.to_json
+            if (id = params[:id].presence)
+              cenit_api_path = "#{cenit_api_path}/#{id}"
+              fields = "#{fields},data"
+            elsif (@query = params[:query].to_s.presence)
+              parameters['$or'] = %w{name title readme summary description}.collect do |field|
+                { field => { '$regex': @query } }
+              end.to_json
             end
-            @objects = []
-            operation.submit(parameters: parameters) do |response|
-              if response.code == 200
-                @response = JSON.parse(response.body)
-                @objects = @response['cross_shared_collections']
+            parameters.merge!(only: fields)
+            operation = Setup::Connection.get(cenit_api_path)
+            if (response = operation.submit(parameters: parameters, verbose_response: true)[:http_response])
+              @response = JSON.parse(response.body)
+              if id
+                if response.code == 200
+                  @object = Setup::CrossSharedCollection.new_from_json(@response)
+                  render :show
+                else
+                  if response.code == 402
+                    flash[:error] = t('admin.flash.object_not_found', model: @model_config.label, id: id)
+                  else
+                    flash[:error] = t("Unable to retrieve #{@model_config.label} with ID #{id}")
+                    redirect_to remote_shared_collection_path
+                  end
+                end
+              else
+                if response.code == 200
+                  @objects = @response['cross_shared_collections']
+                else
+                  flash[:error] = "Unable to retrieve #{@model_config.label_plural} from #{cenit_host}"
+                  redirect_to dashboard_path
+                end
               end
+            else
+              flash[:error] = "Unable to retrieve #{@model_config.label_plural} from #{cenit_host}"
+              redirect_to dashboard_path
             end
-            @model_config = RailsAdmin.config(Setup::CrossSharedCollection)
           end
         end
 
