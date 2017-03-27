65234b38d26aef96df3ae346537f783edf1d7c09
macarci <macarci@gmail.com>
2017-03-26 20:05:54 -0400

    Paginating remote shared collections

--- lib/rails_admin/config/actions/remote_shared_collection.rb
+++ lib/rails_admin/config/actions/remote_shared_collection.rb
@@ -18,15 +18,23 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
-            operation = Setup::Connection.get('http://localhost:3001/api/v2/setup/cross_shared_collection')
-            operation.submit(parameters: {
-              only: 'id,name,title,readme,shared_version,summary,categories'
-            }) do |response|
+            operation = Setup::Connection.get("#{Cenit.host}/api/v2/setup/cross_shared_collection")
+            parameters = {
+              page: params[:page] || 1,
+              only: 'id,name,title,readme,shared_version,summary,categories,description,image',
+              limit: @limit = 20
+            }
+            if (@query = params[:query].to_s.presence)
+              parameters['$or'] = %w{name title readme summary description}.collect { |field| { field => { '$regex': @query } } }.to_json
+            end
+            @objects = []
+            operation.submit(parameters: parameters) do |response|
               if response.code == 200
                 @response = JSON.parse(response.body)
                 @objects = @response['cross_shared_collections']
               end
             end
+            @model_config = RailsAdmin.config(Setup::CrossSharedCollection)
           end
         end
 
