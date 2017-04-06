0d4277b1cd0db74bd70dc7a67534101d2c0fe252
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-04-05 23:42:28 -0400

    update admin foreign notification

--- lib/rails_admin/models/setup/foreign_notification_setting_admin.rb
+++ lib/rails_admin/models/setup/foreign_notification_setting_admin.rb
@@ -12,49 +12,65 @@ module RailsAdmin
               # Email setting vars
               group :email do
                 label 'E-Mail'
+                active { bindings[:object].send_email }
                 field :send_email, :boolean do
                   label 'Send E-Mail?'
                 end
                 field :email_to, :string do
                   label 'To'
+                  required true
                 end
                 field :email_subject, :string do
                   label 'Subject'
+                  required true
                 end
                 field :email_body, :text do
                   label 'Body'
+                  required true
                 end
               end
 
               # Http setting vars
               group :http_request do
                 label 'HTTP Request'
+                active { bindings[:object].send_http_request }
                 field :send_http_request, :boolean do
                   label 'Send HTTP Request?'
                 end
                 field :http_uri, :string do
                   label 'URI'
+                  required true
                 end
                 field :http_method do
                   label 'Method'
+                  required true
                 end
                 field :http_params do
                   label 'Params'
+                  required true
                 end
               end
 
               # SMS setting vars
               group :SMS do
                 label 'SMS'
+                active { bindings[:object].send_sms }
                 field :send_sms, :boolean do
                   label 'Send SMS?'
                 end
                 field :sms_to, :string do
                   label 'To'
+                  required true
                 end
                 field :sms_body, :text do
                   label 'Body'
+                  required true
                 end
+              end
+
+              field :scripts do
+                formatted_value { bindings[:object] }
+                partial 'foreign_notification/form_setting_scripts'
               end
             end
 
