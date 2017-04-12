ea0916bd3384c954c665c2e5187cf7fb12e4540a
Aneli Valdes Acosta <anelivaldes@gmail.com>
2017-04-12 17:27:07 -0400

    configure list_field for PushAdmin

--- lib/rails_admin/models/setup/push_admin.rb
+++ lib/rails_admin/models/setup/push_admin.rb
@@ -14,6 +14,18 @@ module RailsAdmin
               label 'Attempts/Succedded'
             end
 
+            configure :source_collection do
+              list_fields do
+                %w(title image name tags)
+              end
+            end
+
+            configure :shared_collection do
+              list_fields do
+                %w(title image name tags)
+              end
+            end
+
             edit do
               field :description
               field :auto_retry
