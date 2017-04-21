8ed37fa209b1d24b598fdcd7368812c5705c136c
Aneli Valdes Acosta <anelivaldes@gmail.com>
2017-04-21 16:48:57 -0400

    improving  oauth scope field

--- lib/rails_admin/config/fields/types/cenit_oauth_scope.rb
+++ lib/rails_admin/config/fields/types/cenit_oauth_scope.rb
@@ -7,9 +7,82 @@ module RailsAdmin
           register_instance_option :partial do
             :form_cenit_oauth_scope
           end
-
+          register_instance_option :index_pretty_value do
+            "<div class='scope_at_index'> #{pretty_value} </div>".html_safe
+          end
           register_instance_option :pretty_value do
-            value.html_safe if value
+            if value
+              begin
+                cenit_oauth_scope = ::Cenit::OauthScope.new(value).access_by_ids
+                methods = Hash.new { |h, k| h[k] = [] }
+                cenit_oauth_scope.each_criteria do |method, criteria|
+                  methods[method] = Setup::DataType.where(criteria)
+                end
+                basic = Hash.new
+                if !cenit_oauth_scope.openid_set.empty?
+                  basic['openid']= cenit_oauth_scope.openid_set.to_a
+                end
+                if !cenit_oauth_scope.super_methods_set.empty?
+                  basic['super_methods_set']= cenit_oauth_scope.super_methods_set.to_a
+                end
+                if !cenit_oauth_scope.auth?
+                  basic['auth']= true
+                end
+                if !cenit_oauth_scope.offline_access?
+                  basic['offline_access']= true
+                end
+                basic = basic.merge(methods)
+                html = '<ul>'
+                basic.each do |key, value|
+                  case key
+                  when 'openid'
+                    value = value.join(',')
+                    html+= "<li>#{value}</li>"
+                  when 'super_methods_set'
+                    value = value.join(',')
+                    html+= "<li>#{value}</li>"
+                  else
+                    if value == true
+                      html+= "<li>#{key}</li>"
+                    else
+                      next unless (count = value.count) > 0
+                      max_data_type_to_show = 3
+                      config = RailsAdmin.config(::Setup::DataType)
+                      am = config.abstract_model
+                      v = bindings[:view]
+                      if count > max_data_type_to_show
+                        i = 0
+                        first_links =
+                          value.collect do |dt|
+                            if (i < max_data_type_to_show)
+                              label = dt.send(config.object_label_method)
+                              link = v.link_to(label, v.show_path(model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), id: dt.id))
+                            end
+                            i+=1
+                            link
+                          end.to_sentence(options = { last_word_connector: ', ' })
+
+                        message = "<span>Showing data types for #{key} method at #{name}</em></span>"
+                        filter_token = Cenit::Token.create(data: { criteria: value.selector, message: message }, token_span: 1.hours)
+                        index_action = v.action(:index, am)
+                        link_more = v.link_to("more", v.url_for(action: index_action.action_name, model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'),filter_token: filter_token.token), class: 'pjax')
+                        links = "#{first_links} and #{count - max_data_type_to_show} #{link_more}"
+                      else
+                        links = value.collect do |dt|
+                          label = dt.send(config.object_label_method)
+                          v.link_to(label, v.show_path(model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), id: dt.id))
+                        end.to_sentence
+                      end
+                      html+= "<li>#{key}: <span>#{links}</span></li>"
+                    end
+                  end
+                end
+                html += '</ul>'
+                html.html_safe
+              rescue Exception => ex
+                "ERROR: #{ex.message}"
+              end
+            end
           end
 
           register_instance_option :http_methods do
--- lib/rails_admin/models/cenit/oauth_access_grant_admin.rb
+++ lib/rails_admin/models/cenit/oauth_access_grant_admin.rb
@@ -11,60 +11,6 @@ module RailsAdmin
             weight 340
 
             configure :scope, :cenit_oauth_scope
-            show do
-              field :scope do
-                pretty_value do
-                  cenit_oauth_scope = ::Cenit::OauthScope.new(value).access_by_ids
-                  methods = Hash.new { |h, k| h[k] = [] }
-                  cenit_oauth_scope.each_criteria do |method, criteria|
-                    criteria['_id']['$in'].each { |id| methods[method] << id }
-                  end
-                  basic = Hash.new
-                  if !cenit_oauth_scope.openid_set.empty?
-                    basic['openid']= cenit_oauth_scope.openid_set.to_a
-                  end
-                  if !cenit_oauth_scope.super_methods_set.empty?
-                    basic['super_methods_set']= cenit_oauth_scope.super_methods_set.to_a
-                  end
-                  if !cenit_oauth_scope.auth?
-                    basic['auth']= true
-                  end
-                  if !cenit_oauth_scope.offline_access?
-                    basic['offline_access']= true
-                  end
-                  basic = basic.merge(methods)
-                  html = '<ul>'
-                  basic.each do |key, value|
-                    if value == true
-                      html+= "<li>#{key}</li>"
-                      next
-                    end
-                    if key == 'openid'
-                      value = value.join(',')
-                      html+= "<li>#{value}</li>"
-                      next
-                    end
-                    if key == 'super_methods_set'
-                      value = value.join(',')
-                      html+= "<li>#{value}</li>"
-                      next
-                    end
-                    if value.is_a?(Array)
-                      config = RailsAdmin.config(::Setup::DataType)
-                      links = value.collect do |data_type|
-                        next unless (dt = Setup::DataType.where(id: data_type).first)
-                        label = dt.send(config.object_label_method)
-                        (v = bindings[:view]).link_to(label, v.show_path(model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), id: data_type))
-                      end.to_sentence.html_safe
-                      html+= "<li>#{key}: <span>#{links}</span></li>"
-                    end
-                  end
-                  html += '<ul>'
-                  html.html_safe
-                end
-              end
-            end
-
 
             fields :created_at, :application_id, :scope
           end
