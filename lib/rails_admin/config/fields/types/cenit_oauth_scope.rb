c939b41325bde30fb450589f2ef8cb790e83c2ac
Aneli Valdes Acosta <anelivaldes@gmail.com>
2017-04-28 19:56:38 -0400

    Improving Oauth scope description

--- lib/rails_admin/config/fields/types/cenit_oauth_scope.rb
+++ lib/rails_admin/config/fields/types/cenit_oauth_scope.rb
@@ -12,125 +12,7 @@ module RailsAdmin
           end
           register_instance_option :pretty_value do
             if value
-              begin
-                cenit_oauth_scope = ::Cenit::OauthScope.new(value).access_by_ids
-                methods = Hash.new { |h, k| h[k] = [] }
-                cenit_oauth_scope.each_criteria do |method, criteria|
-                  methods[method] = Setup::DataType.where(criteria)
-                end
-                basic = Hash.new
-                if !cenit_oauth_scope.openid_set.empty?
-                  basic['openid']= cenit_oauth_scope.openid_set.to_a
-                  basic['openid'].map! do |d|
-                    case d
-                    when :email
-                      I18n.t('admin.oauth.openid.email')
-                    when :profile
-                      I18n.t('admin.oauth.openid.profile')
-                    when :address
-                      I18n.t('admin.oauth.openid.address')
-                    when :phone
-                      I18n.t('admin.oauth.openid.phone')
-                    else
-                      d
-                    end
-                  end
-                  basic['openid'].delete_if { |a| a == :openid }
-                end
-                if !cenit_oauth_scope.super_methods_set.empty?
-                  basic['super_methods_set']= cenit_oauth_scope.super_methods_set.to_a
-                  basic['super_methods_set'].map! do |d|
-                    case d
-                    when :post
-                      I18n.t('admin.oauth.super_methods.post')
-                    when :get
-                      I18n.t('admin.oauth.super_methods.get')
-                    when :put
-                      I18n.t('admin.oauth.super_methods.put')
-                    when :delete
-                      I18n.t('admin.oauth.super_methods.delete')
-                    else
-                      d
-                    end
-                  end
-                end
-                if !cenit_oauth_scope.auth?
-                  basic['auth']= true
-                end
-                if !cenit_oauth_scope.offline_access?
-                  basic['offline_access']= true
-                end
-                basic = basic.merge(methods)
-                html = '<ul>'
-                basic.each do |key, value|
-                  case key
-                  when 'openid'
-                    value = value.join(',')
-                    html+= "<li>#{value}</li>"
-                  when 'super_methods_set'
-                    value = value.join(',')
-                    html+= "<li>#{value}</li>"
-                  else
-                    if value == true
-                      case key
-                      when 'auth'
-                        key = I18n.t('admin.oauth.auth')
-                      when 'offline_access'
-                        key = I18n.t('admin.oauth.offline_access')
-                      else
-                        key
-                      end
-                      html+= "<li>#{key}</li>"
-                    else
-                      next unless (count = value.count) > 0
-                      max_data_type_to_show = 3
-                      config = RailsAdmin.config(::Setup::DataType)
-                      am = config.abstract_model
-                      v = bindings[:view]
-                      if count > max_data_type_to_show
-                        i = 0
-                        first_links =
-                          value.collect do |dt|
-                            if (i < max_data_type_to_show)
-                              label = dt.send(config.object_label_method)
-                              link = v.link_to(label, v.show_path(model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), id: dt.id))
-                            end
-                            i+=1
-                            link
-                          end.to_sentence(options = { last_word_connector: ', ' })
-
-                        message = "<span>Showing data types for #{key} method at #{name}</em></span>"
-                        filter_token = Cenit::Token.create(data: { criteria: value.selector, message: message }, token_span: 1.hours)
-                        index_action = v.action(:index, am)
-                        link_more = v.link_to("more", v.url_for(action: index_action.action_name, model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), filter_token: filter_token.token), class: 'pjax')
-                        links = "#{first_links} and #{count - max_data_type_to_show} #{link_more}"
-                      else
-                        links = value.collect do |dt|
-                          label = dt.send(config.object_label_method)
-                          v.link_to(label, v.show_path(model_name: ::Setup::DataType.to_s.underscore.gsub('/', '~'), id: dt.id))
-                        end.to_sentence
-                      end
-                      case key
-                      when :get
-                        key =  I18n.t('admin.oauth.access.get')
-                      when :post
-                        key = I18n.t('admin.oauth.access.post')
-                      when :put
-                        key = I18n.t('admin.oauth.access.put')
-                      when :delete
-                        key = I18n.t('admin.oauth.access.delete')
-                      else
-                        key
-                      end
-                      html+= "<li>#{key}: <span>#{links}</span></li>"
-                    end
-                  end
-                end
-                html += "</ul>"
-                html.html_safe
-              rescue Exception => ex
-                "ERROR: #{ex.message}"
-              end
+              (bindings[:view].render partial: 'oauth/description', locals: { scope: value }).html_safe
             end
           end
 
@@ -157,7 +39,11 @@ module RailsAdmin
 
           def parse_input(params)
             if params[name].is_a?(Hash) && !params[name].empty?
-              scope = (params[name].delete('basic') || []).join(' ')
+              basics = params[name].delete('basic') || []
+              if basics.exclude?('openid') && %w(openid email profile address phone).any?{ |n| basics.include?(n)}
+                basics << 'openid'
+              end
+              scope = basics.join(' ')
               methods_hash = Hash.new { |h, k| h[k] = Set.new }
               params[name].each do |data_type_id, methods|
                 next unless methods.is_a?(Array)
@@ -170,7 +56,6 @@ module RailsAdmin
               params[name] = scope
             end
           end
-
         end
       end
     end
