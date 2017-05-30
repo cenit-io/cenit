91483d7cae9aff2d93553f494f681b5c7c4b6b65
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-05-30 13:22:59 -0400

    fix payload bug

--- lib/rails_admin/rest_api/nodejs.rb
+++ lib/rails_admin/rest_api/nodejs.rb
@@ -8,26 +8,28 @@ module RailsAdmin
       def api_nodejs_code(method, path)
         # Get vars definition.
         data, uri, vars = api_data('nodejs', method, path)
+        method = method.to_s.upcase
 
         # Generate uri and command.
         command = ""
         command << "var request = require('request'),\n"
         command << "    #{api_vars('nodejs', vars).gsub("\n", "    \n")}\n" unless vars.empty?
         command << "    options = {\n"
-        command << "      method: '#{method.upcase}',\n"
+        command << "      method: '#{method}',\n"
         command << "      url: '#{uri}',\n"
         command << "      headers: {\n"
         command << "        'Content-Type': 'application/json',\n"
         command << "        'X-User-Access-Key': user_access_key,\n"
         command << "        'X-User-Access-Token': user_access_token\n"
         command << "      },\n"
-        command << "      form: #{data.to_json}\n" unless data.empty?
+        command << "      json: true,\n"
+        command << "      #{(method == 'GET') ? 'qs' : 'body'}: #{data.to_json}\n" unless data.empty?
         command << "    };\n"
         command << "\n"
-        command << "request(options, function (error, response, body) {\n"
+        command << "request(options, function (error, response, data) {\n"
         command << "  if (error) throw error;\n"
         command << "\n"
-        command << "  console.log(JSON.parse(body));\n"
+        command << "  console.log(data);\n"
         command << "});\n"
 
         command
