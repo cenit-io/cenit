a30a19fee02b68d996ee0160fef5172ea4470a5f
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-06-05 12:29:33 -0400

    update rest-api

--- lib/rails_admin/rest_api/j_query.rb
+++ lib/rails_admin/rest_api/j_query.rb
@@ -5,23 +5,23 @@ module RailsAdmin
     module JQuery
       ###
       # Returns jQuery command for service with given method and path.
-      def api_jquery_code(method, path)
+      def api_jquery_code(method, path, with_auth_vars=false)
         # Get vars definition.
-        data, uri, vars = api_data('jquery', method, path)
+        data, uri, vars = api_data('jquery', method, path, with_auth_vars)
 
         # Generate uri and command.
         command = ""
-        command << api_vars('jquery', vars) + "\n\n" unless vars.empty?
+        command << "var #{api_vars('jquery', vars).gsub(/\n/, ",\n    ")};\n\n" unless vars.empty?
         command << "jQuery.ajax({\n"
         command << "  url: '#{uri}',\n"
         command << "  method: '#{method.upcase}',\n"
         command << "  dataType: 'json',\n"
         command << "  headers: {\n"
         command << "    'Content-Type': 'application/json',\n"
-        command << "    'X-User-Access-Key': user_access_key,\n"
-        command << "    'X-User-Access-Token': user_access_token\n"
+        command << "    'X-Tenant-Access-Key': tenent_access_key,\n"
+        command << "    'X-Tenant-Access-Token': tenent_access_token\n"
         command << "  },\n"
-        command << "  data: #{data.to_json}\n" unless data.empty?
+        command << "  data: #{data.to_json},\n" unless data.empty?
         command << "  success: function(data, textStatus, jqXHR) {\n"
         command << "    console.log(data);\n"
         command << "  }\n"
