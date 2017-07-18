bd2742968dc43aacc3f0a3562e1dc7d3085c78b2
macarci <macarci@gmail.com>
2017-07-18 14:44:56 -0400

    Allowing EDI formatters methods for shared collections

--- config/initializers/zcapataz.rb
+++ config/initializers/zcapataz.rb
@@ -18,7 +18,7 @@ Capataz.config do
 
   allow_on Setup::SystemNotification, :create_with
 
-  allow_for Setup::CrossSharedCollection, :pull
+  allow_for Setup::CrossSharedCollection, [:pull, :shared?, :to_json, :share_json, :to_xml, :to_edi]
 
   allow_on [Account, Tenant], [:name, :where, :all, :switch, :notify]
 
