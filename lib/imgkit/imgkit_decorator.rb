e7d5616f1c79a478e5f62fa9f5ad1f85e05e88ab
Alain Fernández Deroncere <alainfd82@gmail.com>
2017-07-20 12:10:10 -0400

    Update the solution for generate PDFs or Images from a URL or HTML

--- lib/imgkit/imgkit_decorator.rb
+++ lib/imgkit/imgkit_decorator.rb
@@ -3,12 +3,12 @@ IMGKit.class_eval do
   def self.image_from_html(url, options = {})
     image = new(url)
     case options[:format]
-      when 'jpg'
-        image.to_jpg
-      when 'jpeg'
-        image.to_jpeg
-      when 'png'
-        image.to_png
+    when 'jpg'
+      image.to_jpg
+    when 'jpeg'
+      image.to_jpeg
+    when 'png'
+      image.to_png
     end
   end
 end
--- lib/pdfkit/pdfkit_decorator.rb
+++ lib/pdfkit/pdfkit_decorator.rb
@@ -5,18 +5,24 @@ require 'cenit/cenit'
 PDFKit.class_eval do
   def self.pdf_from_html(url, options = {})
     header_html = Tempfile.new(%w(header .html))
-    image = Cenit.namespace(options[:namespace]).data_type('images').where(:filename => 'appueste_logo.png').first
-    image_temp = Tempfile.new(%w(appueste_logo .png), :encoding => 'ascii-8bit')
-    image_temp.write(image.data)
-    image_temp.rewind
-    header_html.write(Cenit.namespace(options[:namespace]).translator('header_html').run(:path => image_temp.path))
+    if options[:logo]
+      image = Cenit.namespace(options[:namespace]).data_type('images').where(:filename => options[:logo]).first
+      image_temp = Tempfile.new(%w(logo .png), :encoding => 'ascii-8bit')
+      image_temp.write(image.data)
+      image_temp.rewind
+      header_html.write(Cenit.namespace(options[:namespace]).translator('header_html').run(:path => image_temp.path))
+      image_temp.close
+    else
+      header_html.write(Cenit.namespace(options[:namespace]).snippet('header_html.html.erb').code)
+    end
+
     header_html.rewind
 
     footer_html = Tempfile.new(%w(footer .html))
     footer_html.write(Cenit.namespace(options[:namespace]).snippet('footer_html.html.erb').code)
     footer_html.rewind
 
-    new(
+    pdf = new(
         url,
         :header_html => header_html.path,
         :footer_html => footer_html.path,
@@ -25,5 +31,10 @@ PDFKit.class_eval do
         :margin_left => options[:margin_left],
         :margin_right => options[:margin_right],
     ).to_pdf
+
+    header_html.close
+    footer_html.close
+
+    pdf
   end
 end
