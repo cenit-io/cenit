cdd9c1af737507ed2ebbafa356699b8f4e19c7a9
Aneli Valdes Acosta <anelivaldes@gmail.com>
2017-11-02 18:07:50 -0400

    Added visible lines unchanged before and after changed lines (ins or del) (around_lines_count=2)

--- config/initializers/diffy_monkey_patch.rb
+++ config/initializers/diffy_monkey_patch.rb
@@ -14,11 +14,13 @@ module Diffy
           if is_unchanged l
             unchanged_lines << l
           else
-            if (unchanged_lines.length > min_count_collapse)
-              all_lines.concat(wrapped_collapse(unchanged_lines))
+            if unchanged_lines.length > min_count_collapse
+              first = (all_lines.length == 0)
+              last = (idx +1 == lines.length)
+              all_lines.concat(wrapped_collapse(unchanged_lines, first, last))
               unchanged_lines= []
             else
-              if (unchanged_lines.length > 0)
+              if unchanged_lines.length > 0
                 all_lines.concat(unchanged_lines)
                 unchanged_lines= []
               end
@@ -28,7 +30,9 @@ module Diffy
         end
         if (unchained_count = unchanged_lines.length) > 0
           if unchained_count > min_count_collapse
-            all_lines.concat(wrapped_collapse(unchanged_lines))
+            first = false
+            last = true
+            all_lines.concat(wrapped_collapse(unchanged_lines, first, last))
           else
             all_lines.concat(unchanged_lines)
           end
@@ -45,15 +49,45 @@ module Diffy
       /<li class="(.*)"><span>(.*)<\/span><\/li>/.match(l.lstrip)[2]
     end
 
-    def wrapped_collapse lines
-      [%(<li class="wrapped">
-          <a class="expand_collapse"></a>
-          <span class="count-lines">#{lines.length} lines unchanged </span>
-          <span class="first_line">#{line_code lines.shift}</span>
-          <ul>
-            #{lines.join("\n")}
-          </ul>
-       </li>)]
+    def wrapped_collapse (lines, first, last)
+      around_lines_count = 2
+      total_lines_count = lines.length
+      wrapped_lines_count = total_lines_count
+      before_lines_count = 0
+      before_lines = []
+      after_lines_count = 0
+      after_lines = []
+      if total_lines_count > 2*around_lines_count + 1
+        unless first
+          while before_lines_count < around_lines_count
+            before_lines << lines.shift
+            before_lines_count+=1
+          end
+        end
+        unless last
+          while after_lines_count < around_lines_count
+            after_lines << lines.pop
+            after_lines_count+=1
+          end
+        end
+        wrapped_lines_count = wrapped_lines_count - (before_lines_count + after_lines_count)
+        [%(#{before_lines.join("\n")}
+            <li class="wrapped">
+                      <a class="expand_collapse"></a>
+                      <span class="count-lines">#{wrapped_lines_count} lines unchanged </span>
+                      <span class="first_line">#{line_code lines.shift}</span>
+                      <ul>
+                        #{lines.join("\n")}
+                      </ul>
+            </li>
+            #{after_lines.join("\n")})]
+      else
+        [
+          lines.join("\n")
+        ]
+      end
+
+
     end
 
   end
