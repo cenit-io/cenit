9a631e749c156086883ea09211a959c1dcc57a01
Maikel Arcia Carrazana <macarci@gmail.com>
2018-02-11 17:38:24 -0500

    Home section edition only for configured cenit host

--- lib/rails_admin/models/setup/configuration_admin.rb
+++ lib/rails_admin/models/setup/configuration_admin.rb
@@ -13,7 +13,6 @@ module RailsAdmin
 
             group :data_types do
               label 'Data Types'
-              active false
             end
 
             configure :ecommerce_data_types do
@@ -28,43 +27,48 @@ module RailsAdmin
 
             group :home_sections do
               label 'Home Sections'
-              active false
             end
 
             configure :social_networks, :json_value do
               group :home_sections
               label 'Social Networks'
               help 'Defines your social networks links'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
 
             configure :home_services_menu, :json_value do
               group :home_sections
               label 'Home Service Menu'
               help 'Defines your home services paths'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
 
             configure :home_services, :json_value do
               group :home_sections
               label 'Home Services'
               help 'Describe your home services'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
 
             configure :home_explore_menu, :json_value do
               group :home_sections
               label 'Home Explore Menu'
               help 'Defines your home explore paths'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
 
             configure :home_integrations, :json_value do
               group :home_sections
               label 'Home Integrations'
               help 'Defines your home integrations'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
 
             configure :home_features, :json_value do
               group :home_sections
               label 'Home Features'
               help 'Describe your home features'
+              visible { ::Cenit.host == ::Cenit.homepage }
             end
           end
         end
