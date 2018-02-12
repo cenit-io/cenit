214a02e7999fd5c78a7f3c349b44c35502f84627
Maikel Arcia Carrazana <macarci@gmail.com>
2018-02-11 20:21:46 -0500

    Home sections editable options

--- lib/rails_admin/models/setup/configuration_admin.rb
+++ lib/rails_admin/models/setup/configuration_admin.rb
@@ -34,42 +34,42 @@ module RailsAdmin
               group :home_sections
               label 'Social Networks'
               help 'Defines your social networks links'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['SOCIAL_NETWORKS_SECTION_EDITABLE'].to_b }
             end
 
             configure :home_services_menu, :json_value do
               group :home_sections
               label 'Home Service Menu'
               help 'Defines your home services paths'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['HOME_SERVICE_MENU_SECTION_EDITABLE'].to_b }
             end
 
             configure :home_services, :json_value do
               group :home_sections
               label 'Home Services'
               help 'Describe your home services'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['HOME_SERVICE_SECTION_EDITABLE'].to_b }
             end
 
             configure :home_explore_menu, :json_value do
               group :home_sections
               label 'Home Explore Menu'
               help 'Defines your home explore paths'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['HOME_EXPLORE_MENU_SECTION_EDITABLE'].to_b }
             end
 
             configure :home_integrations, :json_value do
               group :home_sections
               label 'Home Integrations'
               help 'Defines your home integrations'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['HOME_INTEGRATIONS_SECTION_EDITABLE'].to_b }
             end
 
             configure :home_features, :json_value do
               group :home_sections
               label 'Home Features'
               help 'Describe your home features'
-              visible { ::Cenit.host == ::Cenit.homepage }
+              visible { ENV['HOME_FEATURES_SECTION_EDITABLE'].to_b }
             end
           end
         end
