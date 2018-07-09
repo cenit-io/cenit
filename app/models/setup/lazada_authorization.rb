4647bf47fd219ed3b0b316ca480dfcb70d51be5c
Maikel Arcia Carrazana <macarci@gmail.com>
2018-07-09 16:10:34 -0400

    Authorization signing parameters (fixed, again)

--- app/models/setup/lazada_authorization.rb
+++ app/models/setup/lazada_authorization.rb
@@ -10,10 +10,7 @@ module Setup
 
     def token_params(params = {}, template_parameters = {})
       super
-      sign_params(params, template_parameters: {
-        url: client.provider.token_endpoint,
-        skip_access_token: true
-      })
+      sign_params(params, url: client.provider.token_endpoint, skip_access_token: true)
       params
     end
 
