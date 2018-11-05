21ba4b9be180b4719b3e783ee8652221e4213961
Maikel Arcia Carrazana <macarci@gmail.com>
2018-11-05 17:28:43 -0300

    Fixing method visibility

--- app/models/cenit/oauth_scope.rb
+++ app/models/cenit/oauth_scope.rb
@@ -262,6 +262,28 @@ module Cenit
       super_methods.dup
     end
 
+    def can?(action, model)
+      return false unless (data_type = model.try(:data_type))
+      method =
+        case action
+        when :new, :upload_file
+          Cenit::OauthScope::CREATE_TOKEN
+        when :edit, :update
+          Cenit::OauthScope::UPDATE_TOKEN
+        when :index, :show
+          Cenit::OauthScope::READ_TOKEN
+        when :destroy
+          Cenit::OauthScope::DELETE_TOKEN
+        when :digest
+          Cenit::OauthScope::DIGEST_TOKEN
+        else
+          nil
+        end
+      return true if super_method?(method)
+      criteria = access_by_ids.criteria_for(method)
+      criteria.present? && criteria['_id']['$in'].include?(data_type.id.to_s)
+    end
+
     protected
 
     attr_reader :auth, :offline_access, :openid, :access, :super_methods, :multi_tenant
@@ -295,28 +317,6 @@ module Cenit
         (offline_access? ? "#{OFFLINE_ACCESS_TOKEN} " : '') +
         (multi_tenant? ? "#{MULTI_TENANT_TOKEN} " : '') +
         (openid? ? openid.to_a.join(' ') + ' ' : '')).strip
-    end
-
-    def can?(action, model)
-      return false unless (data_type = model.try(:data_type))
-      method =
-        case action
-        when :new, :upload_file
-          Cenit::OauthScope::CREATE_TOKEN
-        when :edit, :update
-          Cenit::OauthScope::UPDATE_TOKEN
-        when :index, :show
-          Cenit::OauthScope::READ_TOKEN
-        when :destroy
-          Cenit::OauthScope::DELETE_TOKEN
-        when :digest
-          Cenit::OauthScope::DIGEST_TOKEN
-        else
-          nil
-        end
-      return true if super_method?(method)
-      criteria = access_by_ids.criteria_for(method)
-      criteria.present? && criteria['_id']['$in'].include?(data_type.id.to_s)
     end
   end
 end
