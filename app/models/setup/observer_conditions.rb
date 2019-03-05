dca15c951f5414e614862ce7eb24b48798038ab9
Andres Gago Alonso <andresml@gmail.com>
2019-03-05 13:42:01 -0500

    For MAC

--- app/models/setup/observer_conditions.rb
+++ app/models/setup/observer_conditions.rb
@@ -19,6 +19,83 @@ module Setup
       hash_field :conditions
     end
 
+    def validate_field_condition?(conditions)
+      conditions.each do |field, cond|
+        if match = field.match(/\A\$(.+)/)
+          unless self.respond_to?("apply_#{match[1]}_operator?")
+            errors.add(:conditions, "have an invalid operator #{field} in a field.")
+            return false
+          end
+
+          if field == '$regex'
+            begin
+              Regexp.new(cond)
+            rescue
+              errors.add(:conditions, "have the following error conforming regular expression: #{ex.message}")
+              return false
+            end
+          end
+
+          if cond.is_a?(Array)
+            unless field == '$in' || field == '$nin' || field == '$elemMatch' || field == '$all'
+              errors.add(:conditions, "cannot apply the operator #{field} to a array #{cond}.")
+              return false
+            end
+            unless field == '$elemMatch' && cond.size > 0 && cond.all? {|c| validate_conditions?(c)}
+              errors.add(:conditions, "have invalid array of conditions #{cond} for operator #{field}.")
+              return false
+            end
+          elsif cond.is_a?(Hash)
+            unless field == '$elemMatch' && validate_conditions?(cond)
+              errors.add(:conditions, "have invalid condition #{cond} for operator #{field}.")
+              return false
+            end
+          end
+        else
+          return false
+        end
+      end
+      return true
+    end
+
+    def validate_conditions?(conditions = self.conditions)
+      if conditions.present?
+        conditions.each do |field, cond|
+          if field == '$or' || field == '$and' || field == '$nor'
+            if cond.is_a?(Array) && cond.size > 1
+              return false unless cond.all? {|c| validate_conditions?(c)}
+            else
+              errors.add(:conditions, "have no arguments for #{field} clause.")
+              return false
+            end
+          elsif field == '$not'
+            if cond.is_a?(Array)
+              if cond.size > 0
+                return false unless cond.all? {|c| validate_conditions?(c)}
+              else
+                errors.add(:conditions, "have no arguments for #{field} clause.")
+                return false
+              end
+            else
+              return false unless validate_conditions?(cond)
+            end
+          else
+            if field.match(/\A\$(.+)/)
+              errors.add(:conditions, "contains #{field} operator non associated to fields.")
+              return false
+            end
+            if cond.is_a?(Hash)
+              return false unless validate_field_condition?(cond)
+            end
+          end
+        end
+      else
+        errors.add(:conditions, "is invalid one.")
+        return false
+      end
+      return true
+    end
+
     def conditions_apply_to?(object_now, object_before = nil, conditions = self.conditions)
       if conditions.present?
         conditions.each do |field, cond|
@@ -28,7 +105,9 @@ module Setup
           elsif field == '$and'
             partial_eval = cond.all? {|c| conditions_apply_to?(object_now, object_before, c)}
           elsif field == '$not'
-            partial_eval = ! conditions_apply_to?(object_now, object_before, cond)
+            partial_eval = !conditions_apply_to?(object_now, object_before, cond)
+          elsif field == '$nor'
+            partial_eval = cond.all? {|c| ! conditions_apply_to?(object_now, object_before, c)}
           else
             values = [object_before && object_before[field], object_now && object_now[field]]
             partial_eval = values && apply?(cond, *values)
@@ -90,7 +169,7 @@ module Setup
     #
     #
     def apply_size_operator?(old_value, new_value, constraint)
-      (! old_value || old_value.size != constraint) && new_value.size == constraint
+      (!old_value || old_value.size != constraint) && new_value.size == constraint
     end
 
     # Evaluator for <tt>regex</tt> operator.
@@ -102,7 +181,7 @@ module Setup
     #
     def apply_regex_operator?(old_value, new_value, constraint)
       regex_pattern = Regexp.new(constraint)
-      (! old_value || ! (old_value =~ regex_pattern)) && new_value =~ regex_pattern
+      (!old_value || !(old_value =~ regex_pattern)) && new_value =~ regex_pattern
     end
 
     # Evaluator for <tt>$elemMatch</tt> operator.
@@ -113,8 +192,8 @@ module Setup
     #
     #
     def apply_elemMatch_operator?(old_value, new_value, constraint)
-      (! old_value || ! (old_value.any? {|c| conditions_apply_to?(c,nil, constraint)})) &&
-      new_value.any? {|c| conditions_apply_to?(c, nil, constraint)}
+      (!old_value || !(old_value.any? {|c| conditions_apply_to?(c, nil, constraint)})) &&
+          new_value.any? {|c| conditions_apply_to?(c, nil, constraint)}
     end
 
     # Evaluator for <tt>$all</tt> operator.
@@ -126,7 +205,7 @@ module Setup
     #
     def apply_all_operator?(old_value, new_value, constraint)
       (!old_value || !(old_value.to_set.superset?(constraint.to_set))) &&
-      new_value.to_set.superset?(constraint.to_set)
+          new_value.to_set.superset?(constraint.to_set)
     end
 
     # Evaluator for <tt>$mod</tt> operator.
@@ -139,6 +218,18 @@ module Setup
     #
     def apply_mod_operator?(old_value, new_value, constraint)
       (!old_value || old_value % constraint[0] == constraint[1]) && new_value % constraint[0] == constraint[1]
+    end
+
+    # Evaluator for <tt>$eq</tt> operator.
+    #
+    # For example:
+    #
+    #    { "color": { "$eq": "yellow"" } }
+    #
+    # applies when a "yellow" <tt>color</tt> attribute takes this value.
+    #
+    def apply_eq_operator?(old_value, new_value, constraint)
+      (!old_value || ! old_value.eql?(constraint)) && new_value.eql?(constraint)
     end
 
     # Evaluator for <tt>$ne</tt> operator.
