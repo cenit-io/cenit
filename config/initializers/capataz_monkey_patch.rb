0d982a70f227cdffdd85bff18f11ae9a3ce33c08
Maikel Arcia Carrazana <macarci@gmail.com>
2019-08-18 08:14:36 -0400

    Add | Clean strategies for Capataz rewritten codes cache

--- config/initializers/capataz_monkey_patch.rb
+++ config/initializers/capataz_monkey_patch.rb
@@ -31,10 +31,12 @@ module Capataz
 
     def rewrite(code, options)
       if Cenit::Redis.client? && (code_key = options[:code_key])
-        code_key = cache_key_for(code_key)
-        unless (code_cache = Cenit::Redis.get(code_key))
+        cache_key = cache_key_for(code_key)
+        unless (code_cache = Cenit::Redis.get(cache_key))
+          clean_strategy.call
           code_cache = Capataz.native_rewrite(code, options)
-          Cenit::Redis.set(code_key, code_cache)
+          Cenit::Redis.set(cache_key, code_cache)
+          Cenit::Redis.zincrby(ZCODES_KEY, 1, code_key)
         end
         code_cache
       else
@@ -42,16 +44,37 @@ module Capataz
       end
     end
 
+    def size
+      Cenit::Redis.zcount(ZCODES_KEY, '1', '+inf')
+    rescue
+      0
+    end
+
     def clean(*args)
       if Cenit::Redis.client?
         keys =
           if args.length > 0
+            Cenit::Redis.zrem(ZCODES_KEY, *args)
             args.map(&method(:cache_key_for))
           else
+            Cenit::Redis.del(ZCODES_KEY)
             Cenit::Redis.keys("#{CODE_PREFIX}*")
           end
         Cenit::Redis.del *keys if keys.count > 0
+        true
+      else
+        false
       end
+    end
+
+    attr_accessor :clean_strategy
+
+    NEVER_CLEAN = proc {}
+
+    BASIC_CLEAN = proc do
+      ::Capataz::Cache.clean if ::Capataz::Cache.size >= 2 * ::Cenit::Rabbit.maximum_active_tasks
     end
   end
 end
+
+Capataz::Cache.clean_strategy = Capataz::Cache::BASIC_CLEAN
