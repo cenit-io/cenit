e631f0cd39130be51b017d13bd3deb7617987096
Maikel Arcia Carrazana <macarci@gmail.com>
2019-08-31 07:09:53 -0400

    Update | Load delayed messages unless SKIP_LOAD_DELAYED_MESSAGES option

--- config/application.rb
+++ config/application.rb
@@ -40,8 +40,7 @@ module Cenit
       next if ENV['SKIP_MONGO_CLIENT'].to_b
       Thread.current[:cenit_initializing] = true
 
-      Setup::DelayedMessage.load_on_start
-      Setup::DelayedMessage.set_load_on_start(ENV['LOAD_DELAYED_MESSAGES_ON_START'].to_b)
+      Setup::DelayedMessage.do_load unless ENV['SKIP_LOAD_DELAYED_MESSAGES'].to_b
 
       puts 'Clearing LOCKS'
       Cenit::Locker.clear
