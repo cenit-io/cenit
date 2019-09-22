ddb9f42e37d2c67d562ebb1cc7dbbdbf4b56b7c9
Miguel Sancho <sanchojaf@gmail.com>
2019-09-22 09:52:00 -0700

    Allow create a new redis instace using different options:
     - with a redis:// URL
     - with connecion variables: host, port, db, password
     - set default values, similar to the default in the redis gem

--- lib/cenit/redis.rb
+++ lib/cenit/redis.rb
@@ -10,7 +10,21 @@ module Cenit
 
       def client
         unless instance_variable_defined?(:@redis_client)
-          client = ::Redis.new(host: ENV["REDIS_HOST"], port: 6379, db: 15)
+          client =
+            if (redis_url = ENV['REDIS_URL'])
+              # specify a connection option as a redis:// URL
+              # e.g "redis://:p4ssw0rd@10.0.1.1:6380/15"
+              ::Redis.new(url: redis_url)
+            else
+              # Defauls values similar to the redis gem
+              # https://github.com/redis/redis-rb/blob/master/lib/redis/client.rb#L10-L26
+              redis_host = ENV['REDIS_HOST'] || '127.0.0.1'
+              redis_port = (ENV['REDIS_PORT'] || 6379).to_i
+              redis_db = (ENV['REDIS_DB'] || 0).to_i
+              redis_password = ENV['REDIS_PASSWORD'] || nil
+              
+              ::Redis.new(host: redis_host, port: redis_port, db: redis_db, password: redis_password)
+            end
           client =
             begin
               client.ping
