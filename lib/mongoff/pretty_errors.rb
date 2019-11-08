d8ee84ac5d14a41cb1569e27e9afa61807b1b8e9
Maikel Arcia Carrazana <macarci@gmail.com>
2019-11-08 18:14:17 -0300

    Fix | Recursive pretty errors

--- lib/mongoff/pretty_errors.rb
+++ lib/mongoff/pretty_errors.rb
@@ -1,7 +1,8 @@
 module Mongoff
   module PrettyErrors
-    def pretty_errors(record)
+    def pretty_errors(record, stack = Set.new)
       return {} unless record
+      stack << record
       errors = record.errors.messages.dup.with_indifferent_access
       if errors.key?(:base) && !property?(:base)
         errors[:'$'] = errors.delete(:base)
@@ -16,17 +17,22 @@ module Mongoff
           end
         if (associations[name.to_s] || associations[name.to_sym]).many?
           record.send(name).each_with_index do |associated, index|
-            next unless (associated_errors = model.pretty_errors(associated)).present?
+            next if stack.include?(associated)
+            next unless (associated_errors = model.pretty_errors(associated, stack)).present?
             errors[name][index] = associated_errors
           end
         else
-          association_errors = model.pretty_errors(record.send(name))
-          if association_errors.present?
-            errors[name].merge!(association_errors)
+          associated = record.send(name)
+          unless stack.include?(associated)
+            association_errors = model.pretty_errors(associated, stack)
+            if association_errors.present?
+              errors[name].merge!(association_errors)
+            end
           end
         end
         errors.delete(name) if errors[name].blank?
       end
+      stack.delete(record)
       errors
     end
   end
