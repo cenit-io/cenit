b9aaee7aff365d65b8befcb032d6ff76e4e0bd84
Maikel Arcia Carrazana <macarci@gmail.com>
2020-10-02 13:28:55 -0300

    Update | Mongoid monkey patch (pilot)

--- config/initializers/mongoid_monkey_patch.rb
+++ config/initializers/mongoid_monkey_patch.rb
@@ -1,7 +1,7 @@
 require 'mongoid/document'
 require 'mongoid/scopable'
 require 'mongoid/factory'
-require 'mongoid/relations/builders/nested_attributes/many'
+#require 'mongoid/relations/builders/nested_attributes/many'
 
 class NilClass
   def tenant_version
@@ -25,10 +25,11 @@ module Mongoid
   module Scopable
     private
 
+    # upgrade ready
     def apply_default_scoping
       if default_scoping
         default_scoping.call.selector.each do |field, value|
-          attributes[field] = value unless field.start_with?('$') || value.respond_to?(:each_pair)
+          attributes[field] = value unless field.start_with?('$') || value.respond_to?(:each)
         end
       end
     end
@@ -43,40 +44,41 @@ module Mongoid
 
     alias_method :mongoid_from_db, :from_db
 
-    def from_db(klass, attributes = nil, selected_fields = nil)
-      mongoid_from_db(klass, attributes, selected_fields).tenant_version
+    def from_db(klass, attributes = nil, criteria = nil, selected_fields = nil)
+      mongoid_from_db(klass, attributes, criteria, selected_fields).tenant_version
     end
   end
 
-  module Relations
-    module Builders
-      module NestedAttributes
-        class Many
-
-          def process_attributes(parent, attrs)
-            return if reject?(parent, attrs)
-            doc = not_found = nil
-            if (id = attrs.extract_id)
-              first = existing.first
-              converted = first ? convert_id(first.class, id) : id
-              begin
-                doc = existing.find(converted)
-              rescue Mongoid::Errors::DocumentNotFound => not_found
-              end
-            end
-            if doc
-              if destroyable?(attrs)
-                destroy(parent, existing, doc)
-              else
-                update_document(doc, attrs)
-              end
-            else
-              raise not_found unless not_found.nil? || metadata.embedded?
-              existing.push(Factory.build(metadata.klass, attrs)) unless destroyable?(attrs)
-            end
-          end
-        end
-      end
-    end
-  end
+  # TODO Upgrading: Check process nested attributes monkey patch
+  # module Relations
+  #   module Builders
+  #     module NestedAttributes
+  #       class Many
+  #
+  #         def process_attributes(parent, attrs)
+  #           return if reject?(parent, attrs)
+  #           doc = not_found = nil
+  #           if (id = attrs.extract_id)
+  #             first = existing.first
+  #             converted = first ? convert_id(first.class, id) : id
+  #             begin
+  #               doc = existing.find(converted)
+  #             rescue Mongoid::Errors::DocumentNotFound => not_found
+  #             end
+  #           end
+  #           if doc
+  #             if destroyable?(attrs)
+  #               destroy(parent, existing, doc)
+  #             else
+  #               update_document(doc, attrs)
+  #             end
+  #           else
+  #             raise not_found unless not_found.nil? || metadata.embedded?
+  #             existing.push(Factory.build(metadata.klass, attrs)) unless destroyable?(attrs)
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
