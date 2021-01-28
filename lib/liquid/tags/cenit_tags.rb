434b6bb2cceac96296a3bb5c4c5653bbf4389692
Maikel Arcia Carrazana <macarci@gmail.com>
2021-01-28 15:40:10 -0300

    Delete | Legacy AWS Auth

--- lib/liquid/tags/cenit_tags.rb
+++ lib/liquid/tags/cenit_tags.rb
@@ -1,7 +1,6 @@
 require 'liquid/tags/eval'
 require 'liquid/tags/base64'
 require 'liquid/tags/oauth_authorization'
-require 'liquid/tags/aws_sign'
 require 'liquid/tags/md5_digest'
 require 'liquid/tags/fail'
 require 'liquid/tags/assign_eval'
