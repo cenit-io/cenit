4043bc3f2e7685dd9ffff00f597a8bfab4182aa1
Tarek N. Elsamni <tarek.samni@gmail.com>
2021-01-30 12:31:41 +0000

    Support RAILS_LOG_TO_STDOUT configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,10 @@ Cenit::Application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  end
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
