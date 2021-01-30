cadaff2d8f08b75741be7caf70072508c1f71b69
Maikel Arcia Carrazana <macarci@gmail.com>
2021-01-30 10:29:54 -0300

    Fix | ENV fetch

--- lib/cenit/redis.rb
+++ lib/cenit/redis.rb
@@ -19,8 +19,8 @@ module Cenit
               # Defauls values similar to the redis gem
               # https://github.com/redis/redis-rb/blob/master/lib/redis/client.rb#L10-L26
               redis_host = ENV.fetch('REDIS_HOST', '127.0.0.1')
-              redis_port = ENV('REDIS_PORT', 6379).to_i
-              redis_db = ENV('REDIS_DB', 0).to_i
+              redis_port = ENV.fetch('REDIS_PORT', 6379).to_i
+              redis_db = ENV.fetch('REDIS_DB', 0).to_i
               redis_password = ENV['REDIS_PASSWORD']
 
               ::Redis.new(host: redis_host, port: redis_port, db: redis_db, password: redis_password)
