02588750e5430a1a3ddfac7b8709e29e9249becc
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-23 18:35:40 -0300

    Update | Generic authorization client config

--- app/models/setup/generic_authorization_client.rb
+++ app/models/setup/generic_authorization_client.rb
@@ -5,7 +5,11 @@ module Setup
     include WithTemplateParameters
     include AuthorizationClientCommon
 
-    build_in_data_type.including(:provider).referenced_by(:_type, :provider, :name).protecting(:identifier, :secret)
+    build_in_data_type
+      .including(:provider)
+      .referenced_by(:_type, :provider, :name)
+      .protecting(:identifier, :secret)
+      .and(label: '{{provider.namespace}} | {{provider.name}} | {{name}}')
 
     parameters :template_parameters
 
