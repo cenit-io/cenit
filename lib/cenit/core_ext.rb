8573ef3b1f712f180ffdcf9193f296635eefb1ca
Maikel Arcia Carrazana <macarci@gmail.com>
2021-03-03 22:12:48 -0300

    Add | Mongoid string evolves for custom ObjectId syntax

--- lib/cenit/core_ext.rb
+++ lib/cenit/core_ext.rb
@@ -53,10 +53,10 @@ class Hash
     each_pair do |key, value|
       if yield(self, key, value)
         case value
-        when Hash
-          value.each_deep_pair(&block)
-        when Array
-          value.each { |sub_value| sub_value.each_deep_pair(&block) if sub_value.is_a?(Hash) }
+          when Hash
+            value.each_deep_pair(&block)
+          when Array
+            value.each { |sub_value| sub_value.each_deep_pair(&block) if sub_value.is_a?(Hash) }
         end
       end
     end if block
@@ -119,7 +119,7 @@ class Numeric
       d: 24
     }
     current_metric = metric
-    h.keys.each do|m|
+    h.keys.each do |m|
       h.delete(m)
       break if m == current_metric
     end
@@ -141,7 +141,25 @@ class Numeric
   end
 end
 
+require 'mongoid/criteria/queryable/extensions/string'
+
 class String
+
+  class << self
+    alias_method :mongoid_evolve, :evolve
+
+    def evolve(object)
+      if object.is_a?(String) && (match = object.match(/\A\$oid#(.*\Z)/))
+        begin
+          BSON::ObjectId.from_string(match[1])
+        rescue
+          mongoid_evolve(object)
+        end
+      else
+        mongoid_evolve(object)
+      end
+    end
+  end
 
   def to_title
     title =
@@ -209,12 +227,12 @@ class String
     end
     taken_method =
       case taken
-      when Hash
-        :key?
-      when Enumerable
-        :include?
-      else
-        nil
+        when Hash
+          :key?
+        when Enumerable
+          :include?
+        else
+          nil
       end
     if taken_method && taken.send(taken_method, str)
       i = 1
@@ -351,16 +369,16 @@ module MIME
           category, subtype = entity.contentType.split('/')
           entity =
             case category
-            when 'audio'
-              MIME::Audio
-            when 'image'
-              MIME::Image
-            when 'text'
-              MIME::Text
-            when 'video'
-              MIME::Video
-            else
-              MIME::Application
+              when 'audio'
+                MIME::Audio
+              when 'image'
+                MIME::Image
+              when 'text'
+                MIME::Text
+              when 'video'
+                MIME::Video
+              else
+                MIME::Application
             end.new(entity.data, subtype, { 'Content-Type' => entity.contentType, 'name' => entity.filename })
         end
         super
@@ -446,7 +464,7 @@ class Time # :nodoc:
   end
 end
 
-class DateTime  # :nodoc:
+class DateTime # :nodoc:
   def to_bson_id
     BSON::ObjectId.from_string(to_i.to_s(16) + '0000000000000000')
   end
