b562090abf35160102e33ec78e417c2d05b4aae2
Maikel Arcia Carrazana <macarci@gmail.com>
2021-03-10 11:27:30 -0300

    Fix | CORS check

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -3,9 +3,9 @@ class FileController < ApplicationController
   include OAuth2AccountAuthorization
   include CorsCheck
 
-  before_action :check_user_signed_in
   before_action :allow_origin_header
   before_action :soft_authorize_account, except: [:cors_check]
+  before_action :check_user_signed_in, except: [:cors_check]
 
   def index
     model = nil
