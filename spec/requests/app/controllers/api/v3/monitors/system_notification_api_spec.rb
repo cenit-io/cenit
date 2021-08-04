443df6dc234b0d03a01a226ef77f56937caaad9d
Henry Dexter <henrydexter9012@gmail.com>
2021-08-04 01:20:13 -0400

    Adding  initials tests for monitors section

---
+++ spec/requests/app/controllers/api/v3/monitors/system_notification_api_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+RSpec.describe Api::V3::ApiController, type: :request do
+
+  describe "GET /api/v3/setup/system_notification" do
+    let(:response_data) {JSON.parse(response.body, symbolize_names: true)}
+
+    context "Fail retrieve all existing System notifications"  do
+      it "with missing header request" do
+        get '/api/v3/setup/system_notification', as: :json
+        expect(response).to have_http_status(:forbidden)
+        expect(response_data[:error]).to eq("insufficient_scope")
+        expect(response_data[:error_description]).to eq("The requested action is out of the access token scope")
+      end
+
+      it "with unauthorized token request"  do
+        get '/api/v3/setup/system_notification', headers: {
+          Authorization: "Bearer unauthorized_token }"
+        }
+        expect(response).to have_http_status(:unauthorized)
+        expect(response_data[:error]).to eq("invalid_token")
+        expect(response_data[:error_description]).to eq("Malformed authorization header")
+      end  
+    end
+
+    context "successful retrieve all existing System notifications"  do
+      let(:access_token) do
+        app = ::Setup::Application.create!(namespace: 'Test', name: 'System notifications')
+        access_grant = ::Cenit::OauthAccessGrant.create!(
+            application_id: app.application_id,
+            scope: 'read'
+        )
+
+        test_user = ::User.current
+        access = ::Cenit::OauthAccessToken.for(
+          app.application_id,
+          access_grant.scope,
+          test_user
+        )
+
+         return token = {
+          Authorization: "Bearer #{access[:access_token]}"
+        }
+      end
+
+      it "with authorized token request"  do
+        get '/api/v3/setup/system_notification', headers: access_token
+        
+        expect(response).to have_http_status(:ok)
+        expect(response_data[:data_type]).to be
+        expect(response_data[:items]).to be
+        expect(response_data[:total_pages]).to be
+      end  
+           
+    end
+  
+  end
+end
