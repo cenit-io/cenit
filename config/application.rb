00d208f2a4581668e50a53823210ddbb5e5d27f0
Maikel Arcia Carrazana <macarci@gmail.com>
2021-09-20 10:53:06 -0300

    Fix | Mongoff config

--- config/application.rb
+++ config/application.rb
@@ -88,25 +88,22 @@ module Cenit
 
       Setup::BuildInFileType.init!
 
-      unless ENV['SKIP_DB_INITIALIZATION'].to_b
-
-        Mongoff::Model.config do
-          before_save ->(record) do
-            record.updated_at = DateTime.now
-            if record.new_record?
-              record.created_at = record.updated_at
-            elsif record.orm_model.observable?
-              record.instance_variable_set(:@_obj_before, record.orm_model.where(id: record.id).first)
-            end
-            true
+      Mongoff::Model.config do
+        before_save ->(record) do
+          record.updated_at = DateTime.now
+          if record.new_record?
+            record.created_at = record.updated_at
+          elsif record.orm_model.observable?
+            record.instance_variable_set(:@_obj_before, record.orm_model.where(id: record.id).first)
           end
+          true
+        end
 
-          after_save ->(record) do
-            if record.orm_model.observable? && !record.instance_variable_get(:@discard_event_lookup)
-              Setup::Observer.lookup(record, record.instance_variable_get(:@_obj_before))
-            end
-            record.remove_instance_variable(:@discard_event_lookup) if record.instance_variable_defined?(:@discard_event_lookup)
+        after_save ->(record) do
+          if record.orm_model.observable? && !record.instance_variable_get(:@discard_event_lookup)
+            Setup::Observer.lookup(record, record.instance_variable_get(:@_obj_before))
           end
+          record.remove_instance_variable(:@discard_event_lookup) if record.instance_variable_defined?(:@discard_event_lookup)
         end
       end
 
@@ -126,7 +123,6 @@ module Cenit
           send("#{association_name}=", values)
         end
       end
-
 
       apps = install_build_in_apps
 
