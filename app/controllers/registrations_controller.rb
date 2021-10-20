4ea353fd793733e8ff52f65e7687ff150946e286
yoandrypa <yoandrypa@gmail.com>
2021-10-20 12:55:20 -0400

    Redirect sign-up action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :allow_x_frame
 
+  def new
+    return super if params[:return_to].to_s =~ /\/app\/ecapi/
+
+    qs = "?#{request.query_string}" if request.query_string.present?
+
+    redirect_to "https://server.cenit.io/users/sign_up#{qs}", status: 301
+  end
+
   def create
     verified = ENV['ENABLE_RERECAPTCHA'].to_b.blank? || ENV['RECAPTCHA_SITE_KEY'].blank? || verify_recaptcha(model: @contact)
     if verified
