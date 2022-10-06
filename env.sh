d45d99eae2fdd68353e4e7d5fc822b9210a797c7
Alejandro Roble Gutierrez <alejandro.r@kiero.co>
2022-10-06 11:16:28 -0400

    Change pattern for build application.yml dynamically. Note: When the container will run need an extra environment variable called ADMIN_UI, this variable is the main URL of front app.

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"" }' ./application.docker.yml >> ./config/application.yml
+awk -F '=' '{ print $1 ": \"" (ENVIRON["ADMIN_UI"] ? ENVIRON["ADMIN_UI"] : $2) "\"" }' ./application.docker.yml >> ./config/application.yml
