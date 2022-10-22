b9fc1bd80a5adf9326586c6b8ccea5880f495df7
Alejandro Roble Gutierrez <arobleg@gmail.com>
2022-10-22 02:48:23 -0400

    Delete application.docker.yml file and create it dynamically

--- env.sh
+++ env.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-awk -F '=' '{ print $1 ": \"" (ENVIRON["ADMIN_UI"] ? ENVIRON["ADMIN_UI"] : $2) "\"" }' ./application.docker.yml >> ./config/application.yml
+cat << EOF > config/application.yml
+HOMEPAGE: ${HOMEPAGE}
+'Cenit::Admin:default_uri': ${ADMIN_UI}
+EOF
